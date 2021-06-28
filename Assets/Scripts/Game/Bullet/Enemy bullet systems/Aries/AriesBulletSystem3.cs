e597b7777e16c07c3cdfb8d954a774f9dac1a957
Nic1493 <nic.choy107@gmail.com>
2021-06-28 19:15:34 -0400

    Enemy ship prefab updates
    attached EnemyBulletSystem scripts to their respective gameobjects

--- Assets/Scripts/Game/Bullet/Enemy bullet systems/Aries/AriesBulletSystem3.cs
+++ Assets/Scripts/Game/Bullet/Enemy bullet systems/Aries/AriesBulletSystem3.cs
@@ -1,6 +1,14 @@
+using System.Collections;
 using UnityEngine;
+using static CoroutineHelper;
 
 public class AriesBulletSystem3 : EnemyBulletSystem
 {
-
+	IEnumerator Start()
+	{
+		while (true)
+		{
+			yield return null;
+		}
+	}
 }
