666b99f2784ae1478579d3e4ebf297f4db4b3353
Nic1493 <nic.choy107@gmail.com>
2021-07-07 22:27:49 -0400

    Player select scripts refactor
    to-do: find a better way to handle pairing input events with stat bar animations

---
+++ Assets/Scripts/Menu/Main menu UI/PlayerStatBar.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class PlayerStatBar : MonoBehaviour
+{
+    Image statBarImage;
+
+    IEnumerator statBarAnimation;
+    const float AnimationDuration = 0.25f;
+    readonly AnimationCurve interpolationCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+    void Awake()
+    {
+        statBarImage = GetComponent<Image>();
+    }
+
+    public void AnimateStatBar(float endFillAmount, Color endColour)
+    {
+        if (statBarAnimation == null)
+        {
+            statBarAnimation = _AnimateStatBar(endFillAmount, endColour);
+            StartCoroutine(statBarAnimation);
+        }
+    }
+
+    IEnumerator _AnimateStatBar(float endFillAmount, Color endColour)
+    {
+        float startFillAmount = statBarImage.fillAmount;
+        Color startColour = statBarImage.color;
+
+        float currentLerpTime = 0f;
+
+        while(currentLerpTime <= AnimationDuration)
+        {
+            float animationProgress = interpolationCurve.Evaluate(currentLerpTime / AnimationDuration);
+
+            statBarImage.fillAmount = Mathf.Lerp(startFillAmount, endFillAmount, animationProgress);
+            statBarImage.color = Color.Lerp(startColour, endColour, animationProgress);
+
+            yield return CoroutineHelper.EndOfFrame;
+            currentLerpTime += Time.deltaTime;
+        }
+
+        statBarAnimation = null;
+    }
+}
---
+++ Assets/Scripts/Menu/Main menu UI/StatBarController.cs
@@ -0,0 +1,68 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class StatBarController : MonoBehaviour
+{
+    EventSystem currentEventSystem;
+    StandaloneInputModule inputModule;
+
+    [SerializeField] ShipObject[] players;
+    [SerializeField] PlayerStatBar[] statBars;
+    float[,] fillAmounts;
+
+    void Awake()
+    {
+        currentEventSystem = EventSystem.current;
+        inputModule = FindObjectOfType<StandaloneInputModule>();
+
+        SetFillAmounts();
+        AnimateStatBars(0);
+    }
+
+    //initialize values in statBarFillAmounts
+    void SetFillAmounts()
+    {
+        //find max <stat> among all player ships
+        float[] maxStatValues = new float[]
+        {
+            players.Select(i => i.Health.OriginalValue).Max(),
+            players.Select(i => i.Power.OriginalValue).Max(),
+            players.Select(i => i.Defense.OriginalValue).Max(),
+            players.Select(i => i.ShootingSpeed.OriginalValue).Max()
+        };
+
+        //set statBarFillAmounts based on max stat values
+        fillAmounts = new float[statBars.Length, players.Length];
+        for (int i = 0; i < fillAmounts.GetLength(0); i++)
+        {
+            fillAmounts[0, i] = players[i].Health.OriginalValue / maxStatValues[0];
+            fillAmounts[1, i] = players[i].Power.OriginalValue / maxStatValues[1];
+            fillAmounts[2, i] = players[i].Defense.OriginalValue / maxStatValues[2];
+            fillAmounts[3, i] = players[i].ShootingSpeed.OriginalValue / maxStatValues[3];
+        }
+
+        //adjust amounts to emphasize stat differences between each other
+        for (int i = 0; i < fillAmounts.GetLength(0); i++)
+        {
+            for (int j = 0; j < fillAmounts.GetLength(1); j++)
+            {
+                fillAmounts[i, j] = (fillAmounts[i, j] - 0.5f) * 2;
+            }
+        }
+    }
+
+    void AnimateStatBars(int selectedPlayerIndex)
+    {
+        for (int i = 0; i < statBars.Length; i++)
+        {
+            statBars[i].AnimateStatBar(fillAmounts[i, selectedPlayerIndex], players[selectedPlayerIndex].UIColour);
+        }
+    }
+
+    void Update()
+    {
+        int currentSelectedIndex = currentEventSystem.currentSelectedGameObject.transform.GetSiblingIndex();
+        AnimateStatBars(currentSelectedIndex);
+    }
+}
