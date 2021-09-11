8a9abbd2cccc00ed01c3229c5dd1db86f85f9fda
Nic1493 <nic.choy107@gmail.com>
2021-09-11 01:03:42 -0400

    Life icon display impl.

---
+++ Assets/Scripts/Game/HUD/LifeCountDisplay.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeCountDisplay : MonoBehaviour
+{
+    [SerializeField] Enemy enemyShip;
+
+    Image[] lifeIcons;
+
+    void Awake()
+    {
+        lifeIcons = GetComponentsInChildren<Image>();
+        enemyShip.LoseLifeAction += OnEnemyLoseLife;
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < lifeIcons.Length; i++)
+        {
+            lifeIcons[i].enabled = i < enemyShip.shipData.MaxLives.Value;
+            lifeIcons[i].color = enemyShip.shipData.UIColour.value;
+        }
+    }
+
+    void OnEnemyLoseLife()
+    {
+        lifeIcons[enemyShip.shipData.CurrentLives.Value - 1].enabled = false;
+    }
+}
