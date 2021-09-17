91469a1f095e39f5b12d54041015d531abd1ece9
Nic1493 <nic.choy107@gmail.com>
2021-09-16 23:09:19 -0400

    AriesBullet1.cs edits

--- Assets/Scripts/Game/Bullet/Enemy bullet systems/Aries/AriesBullet1.cs
+++ Assets/Scripts/Game/Bullet/Enemy bullet systems/Aries/AriesBullet1.cs
@@ -5,9 +5,9 @@ public class AriesBullet1 : EnemyBullet
 {
     protected override IEnumerator Move()
     {
-        yield return StartCoroutine(this.LerpSpeed(2f, -1f, 1f));
+        //yield return StartCoroutine(this.LerpSpeed(2f, 0f, 1f));
 
-        StartCoroutine(this.LerpSpeed(-1f, 2f, 1f));
+        StartCoroutine(this.LerpSpeed(1f, 2f, 1f));
 
         while (enabled)
         {
