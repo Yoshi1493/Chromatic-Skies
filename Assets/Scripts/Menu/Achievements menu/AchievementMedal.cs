298b59f8997cff26a86762af8c7372f1e8408e88
Nic1493 <nic.choy107@gmail.com>
2021-09-30 18:11:40 -0400

    Added achievement medal materials

--- Assets/Scripts/Menu/Achievements menu/AchievementMedal.cs
+++ Assets/Scripts/Menu/Achievements menu/AchievementMedal.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class AchievementMedal : MonoBehaviour
 {
     new Transform transform;
+    [SerializeField] Material[] materials;
 
     void Awake()
     {
         transform = GetComponent<Transform>();
+        GetComponent<MeshRenderer>().material = materials[0];
     }
 
     void Update()
