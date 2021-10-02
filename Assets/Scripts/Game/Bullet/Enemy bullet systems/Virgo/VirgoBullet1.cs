2aff8c8a497b5b4cc1a8be5e1e4cd9f981ff3fdd
Nic1493 <nic.choy107@gmail.com>
2021-10-02 00:50:59 -0400

    Virgo bullet updates
    [VirgoBullet1.asset] updated sprite to a flower petal

--- Assets/Scripts/Game/Bullet/Enemy bullet systems/Virgo/VirgoBullet1.cs
+++ Assets/Scripts/Game/Bullet/Enemy bullet systems/Virgo/VirgoBullet1.cs
@@ -5,10 +5,7 @@ public class VirgoBullet1 : EnemyBullet
 {
     protected override IEnumerator Move()
     {
-        float goldenRatioRadians = ((1 + Mathf.Sqrt(5)) / 2) * Mathf.Rad2Deg;
-
         yield return StartCoroutine(this.LerpSpeed(3f, 1f, 0.5f));
-
-        StartCoroutine(this.LerpSpeed(1f, 3f, 2f));
+        yield return StartCoroutine(this.LerpSpeed(1f, 3f, 2f));
     }
 }
