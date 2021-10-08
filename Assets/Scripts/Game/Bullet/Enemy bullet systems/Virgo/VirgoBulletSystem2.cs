4eedacbe200a99c81b834994907bbce64e86b649
Nic1493 <nic.choy107@gmail.com>
2021-10-08 02:33:58 -0400

    Virgo EBS updates
    [VirgoBulletSystem2.cs] moved attack pattern to VirgoBulletSystem21
    to-do: finish new VirgoBulletSystem2 attack pattern

--- Assets/Scripts/Game/Bullet/Enemy bullet systems/Virgo/VirgoBulletSystem2.cs
+++ Assets/Scripts/Game/Bullet/Enemy bullet systems/Virgo/VirgoBulletSystem2.cs
@@ -1,29 +1,38 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using static CoroutineHelper;
 
 public class VirgoBulletSystem2 : EnemyBulletSystem
 {
+    List<EnemyBullet> bullets = new List<EnemyBullet>(19);
+
     protected override IEnumerator Shoot()
     {
         while (enabled)
         {
             yield return base.Shoot();
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 10; i++)
             {
-                for (int j = 0; j < 45; j++)
-                {
-                    float z = (j * 8f) + (i * 4f);
+                float xPos = (3 * Mathf.Pow((1 - (i * 0.1f)) / 0.8f, 2) - 2 * Mathf.Pow((1 - (i * 0.1f)) / 0.8f, 3)) * 0.3f;
+                float yPos = i / -10f;
 
-                    SpawnBullet(3, z, Vector2.zero).Fire();
-                    SpawnBullet(4, z, Vector2.zero).Fire();
-                }
+                bullets.Add(SpawnBullet(0, 0f, 2 * new Vector3(xPos, yPos, 0f)));
+                bullets.Add(SpawnBullet(0, 0f, 2 * new Vector3(-xPos, yPos, 0f)));
 
-                yield return WaitForSeconds(ShootingCooldown * 8f);
+                yield return WaitForSeconds(ShootingCooldown / 4f);
             }
 
-            yield return ownerShip.MoveToRandomPosition(1f, 1f);
+            bullets.Add(SpawnBullet(0, 0f, 2 * Vector3.down));
+
+            yield return WaitForSeconds(1f);
+
+            bullets.ForEach(b => b.Fire());
+
+            //SetSubsystemEnabled(1, true);            
+
+            yield return ownerShip.MoveToRandomPosition(1f, 5f);
         }
     }
 }
--- Assets/Scripts/Game/Bullet/Enemy bullet systems/Virgo/VirgoBulletSystem21.cs
+++ Assets/Scripts/Game/Bullet/Enemy bullet systems/Virgo/VirgoBulletSystem21.cs
@@ -6,6 +6,19 @@ public class VirgoBulletSystem21 : EnemyBulletSubsystem
 {
     protected override IEnumerator Shoot()
     {
-        yield return WaitForSeconds(3f);
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 30; j++)
+            {
+                float z = (j * 12f) + (i * 6f);
+
+                SpawnBullet(3, z, Vector2.zero).Fire();
+                SpawnBullet(4, z, Vector2.zero).Fire();
+            }
+
+            yield return WaitForSeconds(ShootingCooldown * 8f);
+        }
+
+        enabled = false;
     }
 }
