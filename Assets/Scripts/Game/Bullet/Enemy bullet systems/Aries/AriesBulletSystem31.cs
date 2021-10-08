f1b61ebf12f344740cb277a8e913351442275743
Nic1493 <nic.choy107@gmail.com>
2021-10-08 15:32:56 -0400

    Update AriesBulletSystem31.cs
    changed bullets[] to a List

--- Assets/Scripts/Game/Bullet/Enemy bullet systems/Aries/AriesBulletSystem31.cs
+++ Assets/Scripts/Game/Bullet/Enemy bullet systems/Aries/AriesBulletSystem31.cs
@@ -1,10 +1,11 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using static CoroutineHelper;
 
 public class AriesBulletSystem31 : EnemyBulletSubsystem
 {
-    EnemyBullet[] bullets = new EnemyBullet[28];
+    List<EnemyBullet> bullets = new List<EnemyBullet>(28);
 
     protected override IEnumerator Shoot()
     {
@@ -13,7 +14,7 @@ public class AriesBulletSystem31 : EnemyBulletSubsystem
         for (int i = 0; i < 12; i++)
         {
             float z = i * -30f + 90f;
-            bullets[i] = SpawnBullet(4, z, transform.up.RotateVectorBy(z + 90f) * 2f);
+            bullets.Add(SpawnBullet(4, z, transform.up.RotateVectorBy(z + 90f) * 2f));
 
             yield return WaitForSeconds(ShootingCooldown / 4f);
         }
@@ -23,14 +24,11 @@ public class AriesBulletSystem31 : EnemyBulletSubsystem
         for (int i = 0; i < 16; i++)
         {
             float z = i * 22.5f - 90f;
-            bullets[i + 12] = SpawnBullet(5, z, transform.up.RotateVectorBy(z - 90f) * 2.5f);
+            bullets.Add(SpawnBullet(5, z, transform.up.RotateVectorBy(z - 90f) * 2.5f));
 
             yield return WaitForSeconds(ShootingCooldown / 4f);
         }
 
-        for (int i = 0; i < bullets.Length; i++)
-        {
-            bullets[i].Fire();
-        }
+        bullets.ForEach(b => b.Fire());
     }
 }
