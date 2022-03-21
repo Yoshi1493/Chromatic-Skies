9d28c87857b26da5380bd49d45a44e763c8000d4
Nic1493 <nic.choy107@gmail.com>
2022-03-21 19:10:20 -0400

    Virgo EBS updates
    changed VirgoBulletSystem31.cs to be of type EnemyShooter<EnemyBullet> instead of <Laser>; updated scripts/SOs/prefabs accordingly

--- Assets/Scripts/Game/Bullet/Enemy bullet systems/Virgo/VirgoBulletSystem31.cs
+++ Assets/Scripts/Game/Bullet/Enemy bullet systems/Virgo/VirgoBulletSystem31.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using UnityEngine;
 using static CoroutineHelper;
 
-public class VirgoBulletSystem31 : EnemyShooter<Laser>
+public class VirgoBulletSystem31 : EnemyShooter<EnemyBullet>
 {
     protected override IEnumerator Shoot()
     {
         for (int i = 0; i < 32; i++)
         {
             float z = Mathf.PingPong(i, 8) * 10f - 40f + (i / 8 * 5 - 7.5f);
-            SpawnProjectile(0, z, Vector3.zero);
+            SpawnProjectile(1, z, Vector3.zero).Fire();
 
             yield return WaitForSeconds(ShootingCooldown);
         }
