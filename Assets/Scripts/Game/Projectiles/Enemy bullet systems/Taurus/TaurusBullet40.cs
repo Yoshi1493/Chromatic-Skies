66228d90ec5c4897a07fc52afacb79d1e609e4a9
Nic1493 <nic.choy107@gmail.com>
2022-06-05 18:27:49 -0400

    Taurus EBS updates
    updated AP4

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Taurus/TaurusBullet40.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Taurus/TaurusBullet40.cs
@@ -5,6 +5,7 @@ public class TaurusBullet40 : EnemyBullet
 {
     [SerializeField] bool rotatesClockwise;
 
+    protected override Collider2D CollisionCondition => Physics2D.OverlapBox(transform.position, spriteRenderer.size * 0.8f, transform.eulerAngles.z, CollisionMask);
     protected override float MaxLifetime => 8f;
 
     protected override IEnumerator Move()
