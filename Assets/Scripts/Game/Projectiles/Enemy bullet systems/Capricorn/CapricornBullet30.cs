f84dfd3dfa43e1959cf231c611357503b0fa2c3f
Nic1493 <nic.choy107@gmail.com>
2022-07-29 22:13:48 -0400

    Capricorn EB updates
    updated AP3

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Capricorn/CapricornBullet30.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Capricorn/CapricornBullet30.cs
@@ -2,9 +2,11 @@ using System.Collections;
 
 public class CapricornBullet30 : EnemyBullet
 {
+    protected override float MaxLifetime => 15f;
+
     protected override IEnumerator Move()
     {
         MoveSpeed = 0f;
-        yield return this.LerpSpeed(0f, 3f, 1f, 2f);
+        yield return this.LerpSpeed(0f, 2f, 1f, 2f);
     }
 }
--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Capricorn/CapricornBullet31.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Capricorn/CapricornBullet31.cs
@@ -9,6 +9,6 @@ public class CapricornBullet31 : EnemyBullet
     {
         yield return this.LerpSpeed(3f, 0f, 1f);
         yield return this.RotateBy(60f, 0f, rotatesClockwise, 0.5f);
-        MoveSpeed = 2.5f;
+        MoveSpeed = 2f;
     }
 }
