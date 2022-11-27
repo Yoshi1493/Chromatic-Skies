ce2a39453051ee18968bdcbc6678c6f0a8071d21
Nic1493 <nic.choy107@gmail.com>
2022-11-27 16:03:34 -0500

    Leo EB updates
    updated Leo AP3

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBullet31.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBullet31.cs
@@ -4,6 +4,7 @@ public class LeoBullet31 : EnemyBullet
 {
     protected override IEnumerator Move()
     {
-        yield return this.LerpSpeed(0f, 2f, 1f);
+        yield return this.LerpSpeed(5f, 0.2f, 0.5f);
+        yield return this.LerpSpeed(0.2f, 2f, 1f);
     }
 }
--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBullet32.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBullet32.cs
@@ -6,6 +6,7 @@ public class LeoBullet32 : EnemyBullet
 
     protected override IEnumerator Move()
     {
-        yield return this.LerpSpeed(0f, 1.5f, 1f);
+        yield return this.LerpSpeed(4f, 0.2f, 0.5f);
+        yield return this.LerpSpeed(0.2f, 1.5f, 1f);
     }
 }
