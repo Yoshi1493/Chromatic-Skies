6f3f186cc15b7261ed5b0525cca0f0e452c8399d
Nic1493 <nic.choy107@gmail.com>
2022-11-28 03:37:53 -0500

    Pisces EB, EBS updates
    updated AP5

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Pisces/PiscesBullet50.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Pisces/PiscesBullet50.cs
@@ -1,10 +1,34 @@
 using System.Collections;
 using UnityEngine;
 
-public class PiscesBullet50 : EnemyBullet
+public class PiscesBullet50 : ScriptableEnemyBullet<PiscesBulletSystem5, EnemyBullet>
 {
+    [Space]
+    [SerializeField] ProjectileObject bulletData;
+
+    const int BulletCount = 5;
+    const float BulletSpacing = 15f;
+
+    protected override float MaxLifetime => 1f;
+
     protected override IEnumerator Move()
     {
-        yield return null;
+        yield return this.LerpSpeed(4f, 1f, 0.5f);
+    }
+
+    public override void Destroy()
+    {
+        float t = transform.eulerAngles.z + ownerShip.transform.eulerAngles.z;
+
+        for (int i = 0; i < BulletCount; i++)
+        {
+            float z = (i - ((BulletCount - 1) * 0.5f)) * BulletSpacing + t;
+            Vector3 pos = transform.position;
+
+            bulletData.colour = bulletData.gradient.Evaluate((float)i / BulletCount);
+            SpawnBullet(1, z, pos, false).Fire();
+        }
+
+        base.Destroy();
     }
 }
--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Pisces/PiscesBulletSystem5.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Pisces/PiscesBulletSystem5.cs
@@ -1,38 +1,33 @@
 using System.Collections;
 using UnityEngine;
 using static CoroutineHelper;
-using static MathHelper;
 
 public class PiscesBulletSystem5 : EnemyShooter<EnemyBullet>
 {
-    const int WaveCount = 9;
-    const float WaveSpacing = 2f;
-    const int BranchCount = 20;
-    const int BranchSpacing = 360 / BranchCount;
+    const int WaveCount = 2;
+    const float WaveSpacing = BulletSpacing / 2f;
+    const int BulletCount = 9;
+    const int BulletSpacing = 360 / BulletCount;
 
     protected override IEnumerator Shoot()
     {
         yield return base.Shoot();
 
-        int r = PositiveOrNegativeOne;
-
         while (enabled)
         {
             for (int i = 0; i < WaveCount; i++)
             {
-                for (int ii = 0; ii < BranchCount; ii++)
+                for (int ii = 0; ii < BulletCount; ii++)
                 {
-                    float z = r * ((i * WaveSpacing) + (ii * BranchSpacing));
+                    float z = (i * WaveSpacing) + (ii * BulletSpacing);
                     Vector3 pos = Vector3.zero;
 
                     SpawnProjectile(0, z, pos).Fire();
                 }
 
-                yield return WaitForSeconds(ShootingCooldown);
+                yield return WaitForSeconds(ShootingCooldown * 5f);
+                yield return null;
             }
-
-            yield return WaitForSeconds(ShootingCooldown * 5f);
-            r *= -1;
         }
     }
 }
