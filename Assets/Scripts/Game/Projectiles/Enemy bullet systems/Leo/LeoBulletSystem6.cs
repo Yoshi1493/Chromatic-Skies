b730f59313c90aee893d6cf53d42da0764321013
Nic1493 <nic.choy107@gmail.com>
2022-12-09 00:51:52 -0500

    Leo EB, EBS updates
    updated AP6

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBulletSystem6.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBulletSystem6.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using UnityEngine;
 using static CoroutineHelper;
+using static MathHelper;
 
 public class LeoBulletSystem6 : EnemyShooter<EnemyBullet>
 {
@@ -8,20 +10,24 @@ public class LeoBulletSystem6 : EnemyShooter<EnemyBullet>
     const int BranchCount = 15;
     const float BranchSpacing = 360f / BranchCount;
 
+    protected override float ShootingCooldown => 0.25f;
+
     protected override IEnumerator Shoot()
     {
         yield return base.Shoot();
 
         while (enabled)
         {
+            float r = RandomAngleDeg;
+
             for (int i = 0; i < WaveCount; i++)
             {
                 for (int j = 0; j < BranchCount; j++)
                 {
                     float z = (i * WaveSpacing) + (j * BranchSpacing);
+                    Vector3 pos = transform.up.RotateVectorBy(-z + r);
 
-                    SpawnProjectile(0, z, transform.up.RotateVectorBy(-z)).Fire();
-                    SpawnProjectile(1, -z, transform.up.RotateVectorBy(z)).Fire();
+                    SpawnProjectile(0, z, pos).Fire();
                 }
 
                 yield return WaitForSeconds(ShootingCooldown);
