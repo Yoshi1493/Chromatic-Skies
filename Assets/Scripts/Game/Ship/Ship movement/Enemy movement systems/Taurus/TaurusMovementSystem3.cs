4a8ba22b472aba2fc3f6673167f820ca2f28e35e
Nic1493 <nic.choy107@gmail.com>
2022-12-23 23:50:00 -0500

    Ship movement refactor (pt. 3)
    added (empty) movement system scripts to all Enemy prefabs

---
+++ Assets/Scripts/Game/Ship/Ship movement/Enemy movement systems/Taurus/TaurusMovementSystem3.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using UnityEngine;
+using static CoroutineHelper;
+using static EnemyMovementBehaviour;
+
+public class TaurusMovementSystem3 : EnemyMovement
+{
+    protected override IEnumerator Move()
+    {
+        yield return null;
+    }
+}
