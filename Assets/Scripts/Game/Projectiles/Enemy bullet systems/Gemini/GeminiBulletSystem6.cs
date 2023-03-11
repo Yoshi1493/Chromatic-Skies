1bfafa759e44c0bbaabfe956524f555ed1472c81
Yoshi1493 <nic.choy107@gmail.com>
2023-03-11 16:43:15 -0500

    Enemy prefab + base EBS updates
    filled in all serialized fields on all untouched enemy prefabs
    updated all untouched EBS scripts with updated base template

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Gemini/GeminiBulletSystem6.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Gemini/GeminiBulletSystem6.cs
@@ -1,12 +1,25 @@
 using System.Collections;
+using UnityEngine;
+using static CoroutineHelper;
 
 public class GeminiBulletSystem6 : EnemyShooter<EnemyBullet>
 {
+    const int BulletCount = 0;
+
     protected override IEnumerator Shoot()
     {
-		while (enabled)
-		{
-			yield return null;
-		}        
+        yield return base.Shoot();
+
+        while (enabled)
+        {
+            for (int i = 0; i < BulletCount; i++)
+            {
+                float z = 0f;
+                Vector3 pos = Vector3.zero;
+
+                SpawnProjectile(0, z, pos).Fire();
+                yield return WaitForSeconds(ShootingCooldown);
+            }
+        }
     }
 }
