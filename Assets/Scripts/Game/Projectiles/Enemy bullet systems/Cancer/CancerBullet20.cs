dde47909fc45b6bcd749d05e9061807e01da1ee1
Yoshi1493 <nic.choy107@gmail.com>
2023-03-15 02:27:01 -0400

    Cancer EB, EBS, SO updates
    updated AP2

---
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Cancer/CancerBullet20.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using UnityEngine;
+using static CoroutineHelper;
+
+public class CancerBullet20 : ScriptableEnemyBullet<CancerBulletSystem2, EnemyBullet>
+{
+    const float ShootingCooldown = 0.1f;
+
+    protected override float MaxLifetime => 9f;
+
+    protected override IEnumerator Move()
+    {
+        //StartCoroutine(this.RotateBy(180f, MaxLifetime, true));
+        yield return this.LerpSpeed(0f, 2.5f, 1f);
+
+        while (enabled)
+        {
+            int b = Random.value * 3 <= 1 ? 1 : 2;
+            Vector3 o = 0.5f * Random.insideUnitCircle;
+            float z = transform.eulerAngles.z + (Mathf.Sign(o.x) * 90f);
+            Vector3 pos = transform.position + o;
+
+            SpawnBullet(b, z, pos, false).Fire();
+            yield return WaitForSeconds(ShootingCooldown);
+        }
+    }
+}
--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Cancer/CancerBulletSystem2.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Cancer/CancerBulletSystem2.cs
@@ -4,7 +4,8 @@ using static CoroutineHelper;
 
 public class CancerBulletSystem2 : EnemyShooter<EnemyBullet>
 {
-    const int BulletCount = 0;
+    const int BulletCount = 6;
+    const float BulletSpacing = 360f / BulletCount;
 
     protected override IEnumerator Shoot()
     {
@@ -12,14 +13,20 @@ public class CancerBulletSystem2 : EnemyShooter<EnemyBullet>
 
         while (enabled)
         {
+            float r = PlayerPosition.GetRotationDifference(transform.position) + (BulletSpacing / 2f);
+
             for (int i = 0; i < BulletCount; i++)
             {
-                float z = 0f;
+                float z = (i * BulletSpacing) + r;
                 Vector3 pos = Vector3.zero;
 
                 SpawnProjectile(0, z, pos).Fire();
-                yield return WaitForSeconds(ShootingCooldown);
             }
+
+            yield return WaitForSeconds(8f);
+
+            StartMoveAction?.Invoke();
+            yield return WaitForSeconds(2f);
         }
     }
 }
