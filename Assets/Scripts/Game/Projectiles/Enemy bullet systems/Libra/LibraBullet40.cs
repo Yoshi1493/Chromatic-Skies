90a5100f2292c28959639f45f8d944854a1d2edf
Yoshi1493 <nic.choy107@gmail.com>
2023-04-18 16:55:18 -0400

    Libra EB, EBS, SO updates
    updated AP4

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Libra/LibraBullet40.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Libra/LibraBullet40.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public class LibraBullet40 : EnemyBullet
 {
+    [SerializeField] bool rotatesClockwise;
+
     protected override IEnumerator Move()
     {
-        yield return null;
+        yield return this.LerpSpeed(0f, 3f, 1f);
     }
 }
--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Libra/LibraBulletSystem4.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Libra/LibraBulletSystem4.cs
@@ -1,11 +1,18 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using static CoroutineHelper;
 
 public class LibraBulletSystem4 : EnemyShooter<EnemyBullet>
 {
-    protected override float ShootingCooldown => 0.05f;
+    const int BranchCount = 4;
+    const float BranchSpacing = 360f / BranchCount;
+    const int BulletCount = 2;
     const float BulletSpacing = 0.2f;
+
+    List<EnemyBullet> bullets;
+
+    protected override float ShootingCooldown => 0.04f;
 
     protected override IEnumerator Shoot()
     {
@@ -14,19 +21,35 @@ public class LibraBulletSystem4 : EnemyShooter<EnemyBullet>
         BezierCurve followPath = GetComponent<BezierCreator>().curve;
 
         var points = followPath.CalculateEvenlySpacedPoints(BulletSpacing);
+        int waveCount = points.Length;
+
+        bullets = new(waveCount * BranchCount * BulletCount);
 
         while (enabled)
         {
-            for (int i = 0; i < points.Length; i++)
+            float r = Random.Range(0f, BranchSpacing);
+
+            for (int i = 0; i < waveCount; i++)
             {
-                float z = Vector3.zero.GetRotationDifference(points[i].normal);
-                Vector3 pos = points[i].position;
+                for (int ii = 0; ii < BranchCount; ii++)
+                {
+                    for (int iii = 0; iii < BulletCount; iii++)
+                    {
+                        float t = Vector3.zero.GetRotationDifference(points[i].normal);
+                        float z = (ii * BranchSpacing) + r;
+                        Vector3 pos = ((Vector3)points[i].position).RotateVectorBy(z);
 
-                SpawnProjectile(0, z, pos).Fire();
-
+                        bulletData.colour = bulletData.gradient.Evaluate(i / (waveCount - 1f));
+                        bullets.Add(SpawnProjectile(iii, z + t + (iii * 180f), pos));
+                    }
+                }
 
                 yield return WaitForSeconds(ShootingCooldown);
             }
+
+            yield return WaitForSeconds(0.5f);
+
+            bullets.ForEach(b => b.Fire());
 
             yield return WaitForSeconds(10f);
         }
