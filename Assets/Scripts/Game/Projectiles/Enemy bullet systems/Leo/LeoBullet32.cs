ce772ef8e2c75262d316b66e77be361046f33fa2
Yoshi1493 <nic.choy107@gmail.com>
2023-04-23 15:15:58 -0400

    Update LeoBullet32.cs
    removed unnecessary code

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBullet32.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBullet32.cs
@@ -8,7 +8,5 @@ public class LeoBullet32 : EnemyBullet
     {
         MoveSpeed = 3f;
         yield break;
-        yield return this.LerpSpeed(4f, 0.5f, 0.5f);
-        yield return this.LerpSpeed(0.5f, 1.5f, 1f);
     }
 }
