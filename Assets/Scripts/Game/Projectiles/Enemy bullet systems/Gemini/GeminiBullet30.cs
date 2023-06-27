5f284088eda55a4d9c3e42b657020d64b980e9ca
Yoshi1493 <nic.choy107@gmail.com>
2023-06-27 19:28:00 -0400

    Gemini EB, EBS updates
    updated AP3

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Gemini/GeminiBullet30.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Gemini/GeminiBullet30.cs
@@ -3,8 +3,29 @@ using UnityEngine;
 
 public class GeminiBullet30 : EnemyBullet
 {
+    Vector3 rotationPoint;
+    Vector3 rotationAxis;
+    const float RotationSpeed = 360f;
+
+    protected override float MaxLifetime => 4f;
+
     protected override IEnumerator Move()
     {
+        MoveSpeed = 3f;
         yield return null;
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        rotationPoint = Mathf.Sign(transform.position.x) * GeminiBulletSystem3.BranchSpawnOffset * Vector3.right;
+        rotationAxis = Mathf.Sign(transform.position.x) * Vector3.up;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        transform.RotateAround(rotationPoint, rotationAxis, RotationSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Gemini/GeminiBulletSystem3.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Gemini/GeminiBulletSystem3.cs
@@ -4,22 +4,43 @@ using static CoroutineHelper;
 
 public class GeminiBulletSystem3 : EnemyShooter<EnemyBullet>
 {
-    const int BulletCount = 1;
+    const float WaveSpacing = 10f;
+    const int BranchCount = 2;
+    const float BranchSpacing = 360f / BranchCount;
+    public const float BranchSpawnOffset = 5f;
+    const int BulletCount = 2;
+    const float BulletSpacing = 360f / BulletCount;
+    const float SpinRadius = 1f;
+
+    protected override float ShootingCooldown => 0.1f;
 
     protected override IEnumerator Shoot()
     {
         yield return base.Shoot();
 
+        float i = 0f;
+
         while (enabled)
         {
-            for (int i = 0; i < BulletCount; i++)
+            for (int ii = 0; ii < BranchCount; ii++)
             {
-                float z = 0f;
-                Vector3 pos = Vector3.zero;
+                float t = ii * BranchSpacing;
 
-                SpawnProjectile(0, z, pos).Fire();
-                yield return WaitForSeconds(ShootingCooldown);
+                for (int iii = 0; iii < BulletCount; iii++)
+                {
+                    float vx = SpinRadius * Mathf.Sin((i + (iii * BulletSpacing)) * Mathf.Deg2Rad) + BranchSpawnOffset;
+                    float vy = screenHalfHeight;
+                    float vz = SpinRadius * Mathf.Cos((i + (iii * BulletSpacing)) * Mathf.Deg2Rad);
+                    Vector3 pos = new(vx, vy, vz);
+                    MathHelper.RotateVectorBy(ref pos, t);
+                    float z = t;
+
+                    SpawnProjectile(0, z, pos, false).Fire();
+                }
             }
+
+            yield return WaitForSeconds(ShootingCooldown);
+            i += WaveSpacing;
         }
     }
 }
