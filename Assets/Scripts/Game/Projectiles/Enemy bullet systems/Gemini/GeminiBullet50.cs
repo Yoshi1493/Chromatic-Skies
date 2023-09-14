7842e0d99c1ba153e7b72fd63d10e8114171d059
Yoshi1493 <nic.choy107@gmail.com>
2023-09-14 15:55:00 -0400

    Gemini EB, EBS, SO updates
    started AP5

---
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Gemini/GeminiBullet50.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+
+public class GeminiBullet50 : ReflectiveEnemyBullet
+{
+    protected override float MaxLifetime => 15f;
+
+    protected override IEnumerator Move()
+    {
+        MoveSpeed = 3f;
+        yield return null;
+    }
+
+    protected override void HandleReflection(Collider2D coll)
+    {
+        base.HandleReflection(coll);
+
+        MoveSpeed = 1.5f;
+        spriteRenderer.color = projectileData.gradient.Evaluate(1f);
+    }
+}
--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Gemini/GeminiBulletSystem5.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Gemini/GeminiBulletSystem5.cs
@@ -4,22 +4,27 @@ using static CoroutineHelper;
 
 public class GeminiBulletSystem5 : EnemyShooter<EnemyBullet>
 {
-    const int BulletCount = 0;
+    const float WaveSpacing = 15f;
+    const int BranchCount = 5;
+    const float BranchSpacing = 360f / BranchCount;
+
+    protected override float ShootingCooldown => 0.4f;
 
     protected override IEnumerator Shoot()
     {
         yield return base.Shoot();
 
-        while (enabled)
+        for (int i = 0; enabled; i++)
         {
-            for (int i = 0; i < BulletCount; i++)
+            for (int ii = 0; ii < BranchCount; ii++)
             {
-                float z = 0f;
+                float z = -(i * WaveSpacing) + (ii * BranchSpacing);
                 Vector3 pos = Vector3.zero;
 
                 SpawnProjectile(0, z, pos).Fire();
-                yield return WaitForSeconds(ShootingCooldown);
             }
+
+            yield return WaitForSeconds(ShootingCooldown);
         }
     }
 }
