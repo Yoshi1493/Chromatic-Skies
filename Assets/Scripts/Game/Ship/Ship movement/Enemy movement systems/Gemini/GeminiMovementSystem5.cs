23c845294ecffe27dc5677e06d2eef55ac874dbd
Yoshi1493 <nic.choy107@gmail.com>
2023-09-15 23:12:52 -0400

    Gemini MS updates
    updated MP5

--- Assets/Scripts/Game/Ship/Ship movement/Enemy movement systems/Gemini/GeminiMovementSystem5.cs
+++ Assets/Scripts/Game/Ship/Ship movement/Enemy movement systems/Gemini/GeminiMovementSystem5.cs
@@ -3,17 +3,26 @@ using static CoroutineHelper;
 
 public class GeminiMovementSystem5 : EnemyMovement
 {
+    const int MovesBeforeTeleport = 8;
+
     protected override IEnumerator Move()
     {
+        var ownerShip = GetComponentInParent<Enemy>();
+        var twin = FindObjectOfType<GeminiBullet50>();
+
         yield return this.MoveToRandomPosition(2f, 4f, 4f);
 
         while (enabled)
         {
-            //for (int i = 0; i < 10; i++)
-            //{
-                yield return WaitForSeconds(1f);
+            yield return WaitForSeconds(1f);
+
+            for (int i = 0; i < MovesBeforeTeleport; i++)
+            {
                 yield return this.MoveToRandomPosition(1f, 0.5f, 1f);
-            //}
+                yield return WaitForSeconds(1f);
+            }
+
+            ownerShip.transform.position = twin.transform.position;
         }
     }
 }
