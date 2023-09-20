5181ac061ff31d5777a77547cd457b18ece4ca86
Yoshi1493 <nic.choy107@gmail.com>
2023-09-20 14:03:44 -0400

    Pisces EB, EBS, SO updates
    updated AP6

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Pisces/PiscesBullet60.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Pisces/PiscesBullet60.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public class PiscesBullet60 : EnemyBullet
 {
+    protected override float MaxLifetime => 15f;
+
     protected override IEnumerator Move()
     {
-        yield return this.LerpSpeed(0f, 2.5f, 1f);
+        yield return this.LerpSpeed(0f, 2f, 1f);
     }
 }
--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Pisces/PiscesBulletSystem6.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Pisces/PiscesBulletSystem6.cs
@@ -6,7 +6,7 @@ using static CoroutineHelper;
 public class PiscesBulletSystem6 : EnemyShooter<EnemyBullet>
 {
     const int WaveWaveCount = 3;
-    const float WaveWaveSpacing = BranchSpacing / WaveWaveCount;
+    const float WaveWaveSpacing = BranchSpacing / 2;
     const int WaveCount = 12;
     const float WaveSpacing = 360f / WaveCount;
     const int BranchCount = 6;
@@ -44,6 +44,7 @@ public class PiscesBulletSystem6 : EnemyShooter<EnemyBullet>
                             float z = iv * BulletSpacing;
                             Vector3 pos = (BulletSpawnRadius + (i * SpawnRadiusMultiplier)) * Vector3.Lerp(v1, v2, t);
 
+                            bulletData.colour = bulletData.gradient.Evaluate(i / (WaveWaveCount - 1f));
                             bullets.Add(SpawnProjectile(0, z, pos));
                         }
                     }
@@ -52,12 +53,14 @@ public class PiscesBulletSystem6 : EnemyShooter<EnemyBullet>
                 }
             }
 
-            for (int i = 0; i < WaveWaveCount; i++)
+            for (int i = WaveWaveCount - 1; i >= 0; i--)
             {
                 for (int ii = 0; ii < WaveCount * BranchCount * BulletCount; ii++)
                 {
-                    int b = i + (ii * WaveWaveCount);
+                    int b = (i * WaveCount * BranchCount * BulletCount) + ii;
+
                     bullets[b].Fire();
+                    bullets[b].StartCoroutine(bullets[b].RotateBy(Random.Range(-BranchSpacing, BranchSpacing) * 2f, 3f));
                 }
 
                 yield return WaitForSeconds(0.5f);
