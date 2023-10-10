6d7919bafc63360c7eafa1450b345b796db6ed66
Yoshi1493 <nic.choy107@gmail.com>
2023-10-10 03:07:29 -0400

    Update TaurusBulletSystem61.cs
    forgot to include this in last push somehow :skull:

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Taurus/TaurusBulletSystem61.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Taurus/TaurusBulletSystem61.cs
@@ -1,35 +1,9 @@
 using System.Collections;
-using UnityEngine;
-using static CoroutineHelper;
 
-public class TaurusBulletSystem61 : EnemyShooter<EnemyBullet>
+public class TaurusBulletSystem61 : EnemyShooter<Laser>
 {
-    const int WaveCount = 1;
-    const int BranchCount = 2;
-    const float BranchSpacing = 360f / BranchCount;
-
-    protected override float ShootingCooldown => 0.2f;
-
     protected override IEnumerator Shoot()
     {
-        while (enabled)
-        {
-            for (int i = 0; i < WaveCount; i++)
-            {
-                for (int ii = 0; ii < BranchCount; ii++)
-                {
-                    float x = 3f;
-                    float y = screenHalfHeight * 1.1f;
-                    float z = ii * BranchSpacing;
-                    Vector3 pos = new Vector3(x, y).RotateVectorBy(z);
-
-                    SpawnProjectile(1, z, pos, false).Fire();
-                }
-
-                yield return WaitForSeconds(ShootingCooldown);
-            }
-
-            yield return WaitForSeconds(1f);
-        }
+        yield break;
     }
 }
