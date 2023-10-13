8f84db614313580baef0fd31bcb9572a503cb1c0
Yoshi1493 <nic.choy107@gmail.com>
2023-10-13 02:37:45 -0400

    Update LibraBulletSystem4.cs
    update variable naming consistency

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Libra/LibraBulletSystem4.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Libra/LibraBulletSystem4.cs
@@ -6,8 +6,8 @@ using static MathHelper;
 public class LibraBulletSystem4 : EnemyShooter<EnemyBullet>
 {
     const int BulletCount = 10;
-    const float MinSpawnAngle = 5f;
-    const float MaxSpawnAngle = 15f;
+    const float SpawnMinAngle = 5f;
+    const float SpawnMaxAngle = 15f;
 
     protected override float ShootingCooldown => 0.2f;
 
@@ -23,7 +23,7 @@ public class LibraBulletSystem4 : EnemyShooter<EnemyBullet>
             {
                 yield return WaitForSeconds(ShootingCooldown);
 
-                float z = Random.Range(MinSpawnAngle, MaxSpawnAngle) * PositiveOrNegativeOne + 180f;
+                float z = Random.Range(SpawnMinAngle, SpawnMaxAngle) * PositiveOrNegativeOne + 180f;
                 Vector3 pos = Vector3.zero;
                 SpawnProjectile(0, z, pos).Fire();
             }
