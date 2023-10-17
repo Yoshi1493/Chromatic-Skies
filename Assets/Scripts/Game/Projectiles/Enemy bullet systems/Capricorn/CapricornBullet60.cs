f572c7a2af0689b8d5589f07479166550f85025d
Yoshi1493 <nic.choy107@gmail.com>
2023-10-16 23:07:50 -0400

    Capricorn EB, EBS, SO updates
    started AP6 rework

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Capricorn/CapricornBullet60.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Capricorn/CapricornBullet60.cs
@@ -1,14 +1,37 @@
 using System.Collections;
 using UnityEngine;
+using static CoroutineHelper;
 
-public class CapricornBullet60 : EnemyBullet
+public class CapricornBullet60 : ScriptableEnemyBullet<CapricornBulletSystem6, EnemyBullet>
 {
-    protected override int NumCollisions => Physics2D.OverlapBoxNonAlloc(transform.position, spriteRenderer.size * 0.8f, transform.eulerAngles.z, collisionResults, CollisionMask);
+    const int BranchCount = 2;
+    const float BranchSpacing = 360f / BranchCount;
+    const float ShootingCooldown = 0.1f;
 
-    protected override float MaxLifetime => 6f;
+    protected override float MaxLifetime => Mathf.Infinity;
 
     protected override IEnumerator Move()
     {
-        yield return this.LerpSpeed(0f, 3f, 1f);
+        yield return this.LerpSpeed(3f, 0f, 1f);
+        StartCoroutine(Shoot());
+
+        transform.parent = ownerShip.transform;
+        StartCoroutine(this.RotateAround(ownerShip, MaxLifetime, 90f));
+    }
+
+    IEnumerator Shoot()
+    {
+        while (enabled)
+        {
+            for (int i = 0; i < BranchCount; i++)
+            {
+                float z = i * BranchSpacing;
+                Vector3 pos = transform.position;
+
+                SpawnBullet(1, z, pos, false).Fire();
+            }
+
+            yield return WaitForSeconds(ShootingCooldown);
+        }
     }
 }
--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Capricorn/CapricornBulletSystem6.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Capricorn/CapricornBulletSystem6.cs
@@ -1,38 +1,23 @@
 using System.Collections;
 using UnityEngine;
-using static CoroutineHelper;
 
 public class CapricornBulletSystem6 : EnemyShooter<EnemyBullet>
 {
-    const int WaveCount = 24;
-    const float WaveSpacing = 360f / WaveCount;
-    const int BranchCount = 2;
-    const float BranchSpacing = 360f / BranchCount;
-
-    protected override float ShootingCooldown => 0.05f;
+    const int BulletCount = 2;
+    const float BulletSpacing = 360f / BulletCount;
 
     protected override IEnumerator Shoot()
     {
         yield return base.Shoot();
 
-        SetSubsystemEnabled(1);
+        //SetSubsystemEnabled(1);
 
-        while (enabled)
+        for (int i = 0; i < BulletCount; i++)
         {
-            for (int i = 0; i < WaveCount; i++)
-            {
-                for (int ii = 0; ii < BranchCount; ii++)
-                {
-                    float r = (ii * BranchSpacing);
-                    float z = (i * WaveSpacing) + r;
-                    Vector3 pos = 3f * Vector3.right.RotateVectorBy(r);
+            float z = i * BulletSpacing;
+            Vector3 pos = Vector3.zero;
 
-                    bulletData.colour = bulletData.gradient.Evaluate(i / (WaveCount - 1f) / BranchCount);
-                    SpawnProjectile(0, z, pos).Fire();
-                }
-
-                yield return WaitForSeconds(ShootingCooldown);
-            }
+            SpawnProjectile(0, z, pos).Fire();
         }
     }
 }
