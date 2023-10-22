18c16fd941ce3ab986e228c1db657748a6f79061
Yoshi1493 <nic.choy107@gmail.com>
2023-10-22 15:52:56 -0400

    Capricorn EB updates
    updated AP6

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Capricorn/CapricornBullet60.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Capricorn/CapricornBullet60.cs
@@ -8,8 +8,8 @@ public class CapricornBullet60 : EnemyBullet
     protected override IEnumerator Move()
     {
         float endSpeed = MoveSpeed;
-        yield return this.LerpSpeed(endSpeed, 0f, 0.5f);
+        yield return this.LerpSpeed(-1f, 0f, 1f);
         yield return WaitForSeconds(2f);
-        yield return this.LerpSpeed(0f, -endSpeed, 1f);
+        yield return this.LerpSpeed(0f, endSpeed, 1f);
     }
 }
