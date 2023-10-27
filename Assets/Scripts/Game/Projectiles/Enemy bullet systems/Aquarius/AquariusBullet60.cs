cfb8429681d02957841ca0d399c4b65e8aa297e3
Yoshi1493 <nic.choy107@gmail.com>
2023-10-26 23:25:36 -0400

    Aquarius EB, EBS updates
    updated AP6

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Aquarius/AquariusBullet60.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Aquarius/AquariusBullet60.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using UnityEngine;
+using static CoroutineHelper;
 
 public class AquariusBullet60 : ScriptableEnemyBullet<AquariusBulletSystem6, EnemyBullet>
 {
     [SerializeField] ProjectileObject bulletData;
 
-    const int BulletCount = 24;
+    const int BulletCount = 18;
     const float BulletSpacing = 360f / BulletCount;
 
-    protected override float MaxLifetime => 1f;
+    protected override float MaxLifetime => 3f;
 
     protected override IEnumerator Move()
     {
-        float startSpeed = MoveSpeed;
-        yield return this.LerpSpeed(startSpeed, 0f, 1f);
+        float r = Mathf.PingPong(transform.eulerAngles.z, 180f) * -Mathf.Sign(transform.eulerAngles.z - 180f);
+
+        StartCoroutine(this.RotateBy(r, 1.5f));
+        yield return this.LerpSpeed(4f, 0f, 1f);
+        yield return this.LerpSpeed(0f, 10f, 2f);
+
+        yield return WaitUntil(() => transform.position.y < (-5f - spriteRenderer.size.y));
+        MoveSpeed = 0f;
     }
 
     public override void Destroy()
--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Aquarius/AquariusBulletSystem6.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Aquarius/AquariusBulletSystem6.cs
@@ -1,42 +1,44 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using static CoroutineHelper;
 
 public class AquariusBulletSystem6 : EnemyShooter<EnemyBullet>
 {
-    const int BulletCount = 1;
+    const float ArcHalfWidth = 30f;
+    const int BulletCount = 12;
+    const float BulletSpawnRadius = 0.1f;
 
-    protected override float ShootingCooldown => 0.5f;
+    protected override float ShootingCooldown => 0.05f;
 
     protected override IEnumerator Shoot()
     {
         yield return base.Shoot();
+
+        List<float> bulletSpawnData = new(BulletCount);
 
         StartMoveAction?.Invoke();
         SetSubsystemEnabled(1);
 
         while (enabled)
         {
-            Vector3 v1 = PlayerPosition;
+            for (int i = 0; i < BulletCount; i++)
+            {
+                bulletSpawnData.Add((i % 2 * 2 - 1) * Random.Range(0f, ArcHalfWidth) + 180f);
+            }
+            bulletSpawnData.Randomize();
 
             for (int i = 0; i < BulletCount; i++)
             {
-                float z = 0f;
-                Vector3 pos;
+                float z = bulletSpawnData[i];
+                Vector3 pos = BulletSpawnRadius * Random.insideUnitCircle;
 
-                do
-                {
-                    pos.x = 0.8f * Random.Range(-screenHalfWidth, screenHalfWidth);
-                    pos.y = 0.8f * Random.Range(-screenHalfHeight, screenHalfHeight);
-                    pos.z = 0f;
-                }
-                while (pos.IsTooClose(v1, 2f));
-
-                var bullet = SpawnProjectile(0, z, pos, false);
-                bullet.Fire();
+                SpawnProjectile(0, z, pos).Fire();
+                yield return WaitForSeconds(ShootingCooldown);
             }
 
-            yield return WaitForSeconds(ShootingCooldown);
+            yield return WaitForSeconds(5f);
+            bulletSpawnData.Clear();
         }
     }
 }
