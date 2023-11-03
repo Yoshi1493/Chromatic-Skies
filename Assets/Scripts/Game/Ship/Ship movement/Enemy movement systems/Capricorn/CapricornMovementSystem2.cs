85f91b4654d40e47d04b430507408d68dea3b4eb
Yoshi1493 <nic.choy107@gmail.com>
2023-11-03 01:30:47 -0400

    Capricorn MS updates
    updated MP2

--- Assets/Scripts/Game/Ship/Ship movement/Enemy movement systems/Capricorn/CapricornMovementSystem2.cs
+++ Assets/Scripts/Game/Ship/Ship movement/Enemy movement systems/Capricorn/CapricornMovementSystem2.cs
@@ -1,12 +1,16 @@
 using System.Collections;
+using UnityEngine;
 using static CoroutineHelper;
 
 public class CapricornMovementSystem2 : EnemyMovement
 {
     protected override IEnumerator Move()
     {
+        Vector3 p0 = parentShip.transform.position;
         yield return this.MoveToRandomPosition(1f, 2f, 2f);
-        yield return this.MoveToRandomPosition(1f, 2f, 2f);
+
+        yield return this.MoveTo(p0, 1f);
+
         yield return WaitForSeconds(1f);
         yield return this.MoveToRandomPosition(1f, 3f, 4f);
     }
