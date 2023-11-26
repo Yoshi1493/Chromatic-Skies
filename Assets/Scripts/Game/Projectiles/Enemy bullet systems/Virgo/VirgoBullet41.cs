271205ed06059f2beff9289a733889a191f9c100
Yoshi1493 <nic.choy107@gmail.com>
2023-11-25 23:43:39 -0500

    Virgo EB, EBS, SO updates
    updated AP4

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Virgo/VirgoBullet41.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Virgo/VirgoBullet41.cs
@@ -1,11 +1,43 @@
 using System.Collections;
+using UnityEngine;
 
-public class VirgoBullet41 : EnemyBullet
+public class VirgoBullet41 : ScriptableEnemyBullet<VirgoBulletSystem41, EnemyBullet>
 {
-    protected override float MaxLifetime => 20f;
+    [SerializeField] ProjectileObject bulletData;
+
+    const int BranchCount = 18;
+    const float BranchSpacing = 360f / BranchCount;
+    const int BulletCount = 2;
+    const float BulletStartSpeed = 4f;
+    const float BulletEndSpeed = 1.5f;
+    const float BulletRotationAmount = 60f;
+    const float BulletRotationDuration = 5f;
+    const float ShootingCooldown = 1.5f;
+
+    protected override float MaxLifetime => 4f;
 
     protected override IEnumerator Move()
     {
-        yield return null;
+        yield return this.LerpSpeed(Random.Range(2f, 5f), 0f, MaxLifetime);
+    }
+
+    public override void Destroy()
+    {
+        for (int i = 0; i < BranchCount; i++)
+        {
+            for (int ii = 0; ii < BulletCount; ii++)
+            {
+                float z = i * BranchSpacing;
+                Vector3 pos = transform.position;
+
+                bulletData.colour = bulletData.gradient.Evaluate(ii);
+
+                var bullet = SpawnBullet(2, z, pos, false);
+                bullet.StartCoroutine(bullet.LerpSpeed(BulletStartSpeed, BulletEndSpeed, 2f));
+                bullet.StartCoroutine(bullet.RotateBy((ii % 2 * 2 - 1) * BulletRotationAmount, BulletRotationDuration));
+            }
+        }
+
+        base.Destroy();
     }
 }
--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Virgo/VirgoBulletSystem41.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Virgo/VirgoBulletSystem41.cs
@@ -4,36 +4,20 @@ using static CoroutineHelper;
 
 public class VirgoBulletSystem41 : EnemyShooter<EnemyBullet>
 {
-    const int BranchCount = 18;
-    const float BranchSpacing = 360f / BranchCount;
-    const int BulletCount = 2;
-    const float BulletStartSpeed = 4f;
-    const float BulletEndSpeed = 1.5f;
-    const float BulletRotationAmount = 90f;
-    const float BulletRotationDuration = 5f;
-
     protected override float ShootingCooldown => 1.5f;
 
     protected override IEnumerator Shoot()
     {
         yield return WaitForSeconds(3f);
 
-        while (enabled)
+        for (int i = 1; enabled; i *= -1)
         {
-            for (int i = 0; i < BranchCount; i++)
-            {
-                for (int ii = 0; ii < BulletCount; ii++)
-                {
-                    float z = i * BranchSpacing;
-                    Vector3 pos = Vector3.zero;
+            float x = i * screenHalfWidth * Random.Range(0.5f, 0.8f);
+            float y = screenHalfHeight * 1.1f;
+            float z = 0f;
 
-                    bulletData.colour = bulletData.gradient.Evaluate(ii);
-
-                    var bullet = SpawnProjectile(1, z, pos);
-                    bullet.StartCoroutine(bullet.LerpSpeed(BulletStartSpeed, BulletEndSpeed, 2f));
-                    bullet.StartCoroutine(bullet.RotateBy((ii % 2 * 2 - 1) * BulletRotationAmount, BulletRotationDuration));
-                }
-            }
+            Vector3 pos = new(x, y);
+            SpawnProjectile(1, z, pos, false).Fire();
 
             yield return WaitForSeconds(ShootingCooldown);
         }
