abece4c3c27320282383023a10f4a395afc2a306
Yoshi1493 <nic.choy107@gmail.com>
2023-12-23 17:59:42 -0500

    Update GeminiBulletSystem41.cs
    removed unnecessary comment

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Gemini/GeminiBulletSystem41.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Gemini/GeminiBulletSystem41.cs
@@ -11,8 +11,6 @@ public class GeminiBulletSystem41 : EnemyShooter<EnemyBullet>
     const float BulletSpawnRadius = 1.0f;
     const float SpawnRadiusMultiplier = 0.02f;
 
-    //protected override float ShootingCooldown => 0.05f;
-
     protected override IEnumerator Shoot()
     {
         yield return WaitForSeconds(2f);
