3990a5b86026684cf8ed4aa076dc1dae54632f2d
Yoshi1493 <nic.choy107@gmail.com>
2023-12-27 18:51:11 -0500

    Update GeminiMovementSystem1.cs

--- Assets/Scripts/Game/Ship/Ship movement/Enemy movement systems/Gemini/GeminiMovementSystem1.cs
+++ Assets/Scripts/Game/Ship/Ship movement/Enemy movement systems/Gemini/GeminiMovementSystem1.cs
@@ -5,10 +5,10 @@ public class GeminiMovementSystem1 : EnemyMovement
 {
     protected override IEnumerator Move()
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 4; i++)
         {
             yield return this.MoveToRandomPosition(0.5f, 1f, 2f);
-            yield return WaitForSeconds(1f);
+            yield return WaitForSeconds(0.5f);
         }
     }
 }
