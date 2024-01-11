718788a91380b418e04f062fb530cd7412f99893
Yoshi1493 <nic.choy107@gmail.com>
2024-01-10 19:18:46 -0500

    Sagittarius EB, EBS, SO, MS updates
    started AP, MP 5

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Sagittarius/SagittariusBulletSystem5.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Sagittarius/SagittariusBulletSystem5.cs
@@ -4,7 +4,10 @@ using static CoroutineHelper;
 
 public class SagittariusBulletSystem5 : EnemyShooter<EnemyBullet>
 {
-    const int BulletCount = 0;
+    const int WaveCount = 18;
+    const int BulletCount = 12;
+    const float BulletBaseSpeed = 0.5f;
+    const float BulletSpeedModifier = 1.25f;
 
     protected override IEnumerator Shoot()
     {
@@ -12,14 +15,25 @@ public class SagittariusBulletSystem5 : EnemyShooter<EnemyBullet>
 
         while (enabled)
         {
-            for (int i = 0; i < BulletCount; i++)
-            {
-                float z = 0f;
-                Vector3 pos = Vector3.zero;
+            StartMoveAction?.Invoke();
 
-                SpawnProjectile(0, z, pos).Fire();
+            for (int i = 0; i < WaveCount; i++)
+            {
                 yield return WaitForSeconds(ShootingCooldown);
+
+                for (int ii = 0; ii < BulletCount; ii++)
+                {
+                    float z = 0f;
+                    float s = BulletBaseSpeed + (ii * BulletSpeedModifier);
+                    Vector3 pos = Vector3.zero;
+
+                    var bullet = SpawnProjectile(0, z, pos);
+                    bullet.StartCoroutine(bullet.LerpSpeed(s, 0, 1f, delay: 0.5f));
+                }
             }
+
+            yield return WaitForSeconds(10f);
         }
+
     }
 }
---
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Sagittarius/SagittariusBulletSystem51.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+using static CoroutineHelper;
+
+public class SagittariusBulletSystem51 : EnemyShooter<EnemyBullet>
+{
+    const int BulletCount = 0;
+
+    protected override IEnumerator Shoot()
+    {
+        while (enabled)
+        {
+            for (int i = 0; i < BulletCount; i++)
+            {
+                float z = 0;
+                SpawnProjectile(0, z, Vector3.zero).Fire();
+            }
+
+            yield return WaitForSeconds(ShootingCooldown);
+        }
+    }
+}
