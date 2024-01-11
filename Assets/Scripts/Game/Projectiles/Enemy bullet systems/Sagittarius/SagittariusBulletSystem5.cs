322dd78bd8f5676ac3de8e2b3decb071617a49b8
Yoshi1493 <nic.choy107@gmail.com>
2024-01-10 20:00:40 -0500

    Sagittarius EBS, SO updates
    updated AP5

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Sagittarius/SagittariusBulletSystem5.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Sagittarius/SagittariusBulletSystem5.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using static CoroutineHelper;
 
@@ -7,7 +8,9 @@ public class SagittariusBulletSystem5 : EnemyShooter<EnemyBullet>
     const int WaveCount = 18;
     const int BulletCount = 12;
     const float BulletBaseSpeed = 0.5f;
-    const float BulletSpeedModifier = 1.25f;
+    const float BulletSpeedModifier = 0.75f;
+
+    List<EnemyBullet> bullets = new(WaveCount * BulletCount);
 
     protected override IEnumerator Shoot()
     {
@@ -15,21 +18,45 @@ public class SagittariusBulletSystem5 : EnemyShooter<EnemyBullet>
 
         while (enabled)
         {
+            bullets.Clear();
+
             StartMoveAction?.Invoke();
+            yield return WaitForSeconds(ShootingCooldown);
+
+            Vector3 d = ownerShip.GetComponent<Enemy>().GetCurrentMovementSystem().moveDirection;
+            d *= -Mathf.Sign(d.x);
+
+            float z = d.RotateVectorBy(90f).GetRotationDifference(Vector3.zero);
+            Vector3 pos = Vector3.zero;
 
             for (int i = 0; i < WaveCount; i++)
             {
-                yield return WaitForSeconds(ShootingCooldown);
-
                 for (int ii = 0; ii < BulletCount; ii++)
                 {
-                    float z = 0f;
                     float s = BulletBaseSpeed + (ii * BulletSpeedModifier);
-                    Vector3 pos = Vector3.zero;
+
+                    bulletData.colour = bulletData.gradient.Evaluate(ii / (BulletCount - 1f));
 
                     var bullet = SpawnProjectile(0, z, pos);
-                    bullet.StartCoroutine(bullet.LerpSpeed(s, 0, 1f, delay: 0.5f));
+                    bullet.StartCoroutine(bullet.LerpSpeed(s, 0, 2f, delay: 0.5f));
+                    bullets.Add(bullet);
                 }
+
+                yield return WaitForSeconds(ShootingCooldown);
+            }
+
+            yield return WaitForSeconds(2f);
+
+            for (int i = 0; i < BulletCount; i++)
+            {
+                for (int ii = 0; ii < WaveCount; ii++)
+                {
+                    int b = ii * BulletCount + i;
+
+                    bullets[b].Destroy();
+                }
+
+                yield return WaitForSeconds(ShootingCooldown);
             }
 
             yield return WaitForSeconds(10f);
