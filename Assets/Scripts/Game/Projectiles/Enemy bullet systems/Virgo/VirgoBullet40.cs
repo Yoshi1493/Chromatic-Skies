0f937b628294ec8c4ab2f2a4a99d6f8efb946b55
Yoshi1493 <nic.choy107@gmail.com>
2024-01-18 01:03:53 -0500

    Update VirgoBullet40.cs
    removed unnecessary yield statement

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Virgo/VirgoBullet40.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Virgo/VirgoBullet40.cs
@@ -4,8 +4,6 @@ public class VirgoBullet40 : EnemyBullet
 {
     protected override IEnumerator Move()
     {
-        yield return null;
-
         StartCoroutine(this.LerpSpeed(1f, 4f, 1f));
         yield return this.RotateAround(ownerShip.transform.position, 1.5f, 270f, delay: 0.5f);
         yield return this.LerpSpeed(MoveSpeed, 4f, 1f);
