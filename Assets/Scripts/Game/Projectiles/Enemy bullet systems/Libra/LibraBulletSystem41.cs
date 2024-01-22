70df25dd0b24e53e0cf5efe5e3012bb6cafc2e53
Yoshi1493 <nic.choy107@gmail.com>
2024-01-21 22:54:16 -0500

    Libra EB, EBS, SO updates
    updated AP4

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Libra/LibraBulletSystem41.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Libra/LibraBulletSystem41.cs
@@ -5,7 +5,6 @@ using static CoroutineHelper;
 public class LibraBulletSystem41 : EnemyShooter<Laser>
 {
     const int WaveCount = 4;
-    const float WaveSpacing = LaserSpacing / 2f;
     const int LaserCount = 8;
     const float LaserSpacing = 360f / LaserCount;
     const float LaserSpawnRadius = 0.5f;
@@ -19,10 +18,11 @@ public class LibraBulletSystem41 : EnemyShooter<Laser>
         for (int i = 0; i < WaveCount; i++)
         {
             yield return WaitForSeconds(ShootingCooldown);
+            float r = PlayerPosition.GetRotationDifference(transform.position);
 
             for (int ii = 0; ii < LaserCount; ii++)
             {
-                float z = (i * WaveSpacing) + (ii * LaserSpacing);
+                float z = (ii * LaserSpacing) + r;
                 Vector3 pos = LaserSpawnRadius * Vector3.up.RotateVectorBy(z);
 
                 bulletData.colour = bulletData.gradient.Evaluate(i % 2);
