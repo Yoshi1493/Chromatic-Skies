589afddf7275461ed820964f0fd1311592ab5e96
Yoshi1493 <nic.choy107@gmail.com>
2024-02-02 03:20:02 -0500

    Scorpio EB, EBS, SO updates
    updated AP3

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Scorpio/ScorpioBullet30.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Scorpio/ScorpioBullet30.cs
@@ -1,10 +1,17 @@
 using System.Collections;
-using UnityEngine;
 
 public class ScorpioBullet30 : EnemyBullet
 {
+    protected override float MaxLifetime => ScorpioBulletSystem3.BigBulletRotationDuration;
+
     protected override IEnumerator Move()
     {
-        yield return null;
+        yield return this.LerpSpeed(4f, 2f, MaxLifetime);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        spriteRenderer.color = projectileData.gradient.Evaluate(currentLifetime / MaxLifetime);
     }
 }
--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Scorpio/ScorpioBulletSystem3.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Scorpio/ScorpioBulletSystem3.cs
@@ -1,13 +1,18 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using static CoroutineHelper;
 
 public class ScorpioBulletSystem3 : EnemyShooter<EnemyBullet>
 {
-    const int BulletCount = 30;
-    const float BulletSpacing = 360f / BulletCount;
-    const float BulletRotationSpeed = 45f;
-    public const float BulletRotationDuration = 3f;
+    const int BigBulletCount = 30;
+    const float BigBulletSpacing = 360f / BigBulletCount;
+    const float BigBulletRotationSpeed = 45f;
+    public const float BigBulletRotationDuration = 1f;
+    const float SmallBulletCount = 3;
+    const float SmallBulletSpacing = 360f / SmallBulletCount;
+
+    Queue<EnemyBullet> bullets = new(BigBulletCount);
 
     protected override float ShootingCooldown => 0.5f;
 
@@ -15,16 +20,48 @@ public class ScorpioBulletSystem3 : EnemyShooter<EnemyBullet>
     {
         yield return base.Shoot();
 
-        for(int i = 1; enabled; i *= -1)
+        bullets.Clear();
+
+        for (int i = 1; enabled; i *= -1)
         {
-            for (int ii = 0; ii < BulletCount; ii++)
+            StartCoroutine(BatchDequeue());
+
+            for (int ii = 0; ii < BigBulletCount; ii++)
             {
-                float z = 0f;
+                float z = ii * BigBulletSpacing;
                 Vector3 pos = Vector3.zero;
 
-                SpawnProjectile(0, z, pos).Fire();
-                yield return WaitForSeconds(ShootingCooldown);
+                var bullet = SpawnProjectile(0, z, pos);
+                bullet.StartCoroutine(bullet.RotateBy(i * BigBulletRotationSpeed, BigBulletRotationDuration));
+                bullet.Fire();
+                bullets.Enqueue(bullet);
             }
+
+            yield return WaitForSeconds(ShootingCooldown);
         }
     }
+
+    IEnumerator BatchDequeue()
+    {
+        yield return WaitForSeconds(BigBulletRotationDuration);
+
+        for (int i = 0; i < BigBulletCount; i++)
+        {
+            if (bullets.TryDequeue(out EnemyBullet bullet))
+            {
+                float r = Random.Range(0f, SmallBulletSpacing);
+
+                for (int ii = 0; ii < SmallBulletCount; ii++)
+                {
+                    float z = ii * SmallBulletSpacing + r;
+                    Vector3 pos = bullet.transform.position;
+
+                    SpawnProjectile(1, z, pos, false).Fire();
+                }
+            }
+        }
+
+        yield break;
+    }
+
 }
