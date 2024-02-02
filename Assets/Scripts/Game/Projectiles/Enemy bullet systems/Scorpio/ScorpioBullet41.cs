0df96298c1ebb5e240e426688cfc8c3d5ca78612
Yoshi1493 <nic.choy107@gmail.com>
2024-02-02 01:28:05 -0500

    Update ScorpioBullet41.cs

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Scorpio/ScorpioBullet41.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Scorpio/ScorpioBullet41.cs
@@ -6,6 +6,6 @@ public class ScorpioBullet41 : EnemyBullet
     {
         float startSpeed = MoveSpeed;
         yield return this.LerpSpeed(startSpeed, 0f, 1f);
-        yield return this.LerpSpeed(0f, startSpeed * 1.5f, 2f);
+        yield return this.LerpSpeed(0f, startSpeed, 3f);
     }
 }
