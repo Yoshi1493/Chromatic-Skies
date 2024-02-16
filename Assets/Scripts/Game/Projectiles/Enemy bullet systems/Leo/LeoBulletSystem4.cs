0019c1888faf4994802e695e11d860582b3f7459
Yoshi1493 <nic.choy107@gmail.com>
2024-02-16 18:26:19 -0500

    Leo EB, EBS, SO updates
    updated AP4

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBulletSystem4.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBulletSystem4.cs
@@ -4,11 +4,14 @@ using static CoroutineHelper;
 
 public class LeoBulletSystem4 : EnemyShooter<Laser>
 {
-    const int WaveCount = 4;
-    const int BranchCount = 2;
-    const int LaserCount = 6;
+    const int WaveCount = 2;
+    const int BranchCount = 6;
+    const float BranchSpacing = 15f;
+    const int LaserCount = 2;
+    const float MaxAngleOffset = 45f;
+    const float LaserSpawnRadius = 0.75f;
 
-    protected override float ShootingCooldown => 0.5f;
+    protected override float ShootingCooldown => 2f;
 
     protected override IEnumerator Shoot()
     {
@@ -18,16 +21,25 @@ public class LeoBulletSystem4 : EnemyShooter<Laser>
         {
             for (int i = 0; i < WaveCount; i++)
             {
-                for (int ii = 0; ii < BranchCount; ii++)
+                for (int ii = 1; ii <= BranchCount; ii++)
                 {
+                    float r = Random.Range(-MaxAngleOffset, MaxAngleOffset);
+                    float t = Random.Range(-BranchSpacing, BranchSpacing);
+
                     for (int iii = 0; iii < LaserCount; iii++)
                     {
+                        int d = iii % 2 * 2 - 1;
+                        float z = d * (ii * BranchSpacing + t + 180f);
+                        Vector3 pos = LaserSpawnRadius * -transform.up.RotateVectorBy(z + (d * r));
 
+                        SpawnProjectile(i, z, pos).Fire(1f);
                     }
                 }
 
                 yield return WaitForSeconds(ShootingCooldown);
             }
+
+            yield return WaitForSeconds(10f);
         }
     }
 }
