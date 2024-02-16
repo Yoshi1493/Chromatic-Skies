907795ce9c77c19fc5e16f9995cc7ac2be77020a
Yoshi1493 <nic.choy107@gmail.com>
2024-02-16 14:37:20 -0500

    Leo EBS updates
    started AP4

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBulletSystem4.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBulletSystem4.cs
@@ -1,12 +1,33 @@
 using System.Collections;
+using UnityEngine;
+using static CoroutineHelper;
 
-public class LeoBulletSystem4 : EnemyShooter<EnemyBullet>
+public class LeoBulletSystem4 : EnemyShooter<Laser>
 {
+    const int WaveCount = 4;
+    const int BranchCount = 2;
+    const int LaserCount = 6;
+
+    protected override float ShootingCooldown => 0.5f;
+
     protected override IEnumerator Shoot()
     {
-		while (enabled)
-		{
-			yield return null;
-		}        
+        yield return base.Shoot();
+
+        while (enabled)
+        {
+            for (int i = 0; i < WaveCount; i++)
+            {
+                for (int ii = 0; ii < BranchCount; ii++)
+                {
+                    for (int iii = 0; iii < LaserCount; iii++)
+                    {
+
+                    }
+                }
+
+                yield return WaitForSeconds(ShootingCooldown);
+            }
+        }
     }
 }
