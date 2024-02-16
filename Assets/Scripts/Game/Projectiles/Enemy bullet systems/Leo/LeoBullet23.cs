c78bbe56a61d7606bfa7d108e4b26b1ca375475a
Yoshi1493 <nic.choy107@gmail.com>
2024-02-16 01:10:22 -0500

    Leo EB, prefab updates
    updated AP2

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBullet23.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBullet23.cs
@@ -2,12 +2,12 @@ using System.Collections;
 
 public class LeoBullet23 : EnemyBullet
 {
-    protected override float MaxLifetime => 16f;
+    protected override float MaxLifetime => 12f;
 
     protected override IEnumerator Move()
     {
         float endSpeed = MoveSpeed;
         StartCoroutine(this.LerpSpeed(0f, endSpeed, 2f));
-        yield return this.RotateBy(100f, MaxLifetime, false);
+        yield return this.RotateBy(60f, 10f, false);
     }
 }
