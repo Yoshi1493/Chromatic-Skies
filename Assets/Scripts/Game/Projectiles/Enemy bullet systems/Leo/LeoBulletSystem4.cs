629602a4b5838145f554ff43fe7eb8a2072c4f3a
Yoshi1493 <nic.choy107@gmail.com>
2024-02-24 17:56:06 -0500

    Leo EBS, MS updates
    updated AP, MP 4

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBulletSystem4.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBulletSystem4.cs
@@ -5,13 +5,17 @@ using static CoroutineHelper;
 
 public class LeoBulletSystem4 : EnemyShooter<EnemyBullet>
 {
-    const int RepeatCount = 6;
-    const int WaveCount = 5;
-    const float WaveSpacing = 360f / WaveCount;
+    const int WaveCount = 6;
+    const int BranchCount = 5;
+    const float BranchSpacing = 360f / BranchCount;
     const int BulletCount = 12;
+    const float BulletSpacing = 6f;
     const float BulletSpawnRadius = 1.5f;
+    const float SpawnRadiusModifier = 0.15f;
+    const float BulletBaseSpeed = 2f;
+    const float BulletSpeedModifier = 0.15f;
 
-    Queue<EnemyBullet> bullets = new(RepeatCount * WaveCount * BulletCount);
+    Queue<EnemyBullet> bullets = new(WaveCount * BranchCount * BulletCount);
 
     protected override float ShootingCooldown => 1f / 60;
 
@@ -23,33 +27,50 @@ public class LeoBulletSystem4 : EnemyShooter<EnemyBullet>
         {
             //SetSubsystemEnabled(1);
 
-            for (int i = 0; i < RepeatCount; i++)
+            for (int i = 0; i < WaveCount; i++)
             {
                 Vector3 v0 = ownerShip.transform.position;
                 StartMoveAction?.Invoke();
 
-                for (int ii = 0; ii < WaveCount; ii++)
+                for (int ii = 0; ii < BranchCount; ii++)
                 {
-                    Vector3 v1 = Vector3.up.RotateVectorBy(2 * ii * WaveSpacing);
-                    Vector3 v2 = Vector3.up.RotateVectorBy(2 * (ii + 1) * WaveSpacing);
+                    Vector3 v1 = Vector3.up.RotateVectorBy(2 * ii * BranchSpacing);
+                    Vector3 v2 = Vector3.up.RotateVectorBy(2 * (ii + 1) * BranchSpacing);
 
                     for (int iii = 0; iii < BulletCount; iii++)
                     {
-                        float z = 0f;
-                        Vector3 pos = BulletSpawnRadius * Vector3.Lerp(v1, v2, (float)iii / BulletCount) + v0;
+                        Vector3 pos = (BulletSpawnRadius + (i * SpawnRadiusModifier))* Vector3.Lerp(v1, v2, (float)iii / BulletCount) + v0;
+                        float z = v0.GetRotationDifference(pos) + (iii * BulletSpacing);
 
                         bulletData.colour = bulletData.gradient.Evaluate(iii / (BulletCount - 1f));
 
-                        SpawnProjectile(0, z, pos, false).Fire();
+                        bullets.Enqueue(SpawnProjectile(0, z, pos, false));
                         yield return WaitForSeconds(ShootingCooldown);
                     }
                 }
 
-                yield return WaitForSeconds(0.5f);
+                StartCoroutine(FireBullets());
             }
 
             yield return WaitForSeconds(10f);
         }
     }
 
+    IEnumerator FireBullets()
+    {
+        yield return WaitForSeconds(1f);
+
+        for (int i = 0; i < BranchCount; i++)
+        {
+            for (int ii = 0; ii < BulletCount; ii++)
+            {
+                float s = BulletBaseSpeed + (i * BulletSpeedModifier);
+
+                if (bullets.TryDequeue(out EnemyBullet bullet))
+                {
+                    bullet.StartCoroutine(bullet.LerpSpeed(0f, s, 1f));
+                }
+            }
+        }
+    }
 }
--- Assets/Scripts/Game/Ship/Ship movement/Enemy movement systems/Leo/LeoMovementSystem4.cs
+++ Assets/Scripts/Game/Ship/Ship movement/Enemy movement systems/Leo/LeoMovementSystem4.cs
@@ -6,6 +6,6 @@ public class LeoMovementSystem4 : EnemyMovement
     protected override IEnumerator Move()
     {
         yield return WaitForSeconds(0.2f);
-        yield return this.MoveToRandomPosition(1f, 3f, 5f);
+        yield return this.MoveToRandomPosition(0.8f, 3f, 5f);
     }
 }
