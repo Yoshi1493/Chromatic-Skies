1c92c0377f572e3f83b3255a7ce60ff8f246c164
Yoshi1493 <nic.choy107@gmail.com>
2024-03-20 18:56:25 -0400

    Leo EB, EBS, SO updates
    updated AP4

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBullet42.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBullet42.cs
@@ -2,6 +2,8 @@ using System.Collections;
 
 public class LeoBullet42 : EnemyBullet
 {
+    protected override float MaxLifetime => 6f;
+
     protected override IEnumerator Move()
     {
         yield return null;
--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBulletSystem42.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBulletSystem42.cs
@@ -4,15 +4,19 @@ using static CoroutineHelper;
 
 public class LeoBulletSystem42 : EnemyShooter<EnemyBullet>
 {
-    const int WaveCount = 54;
-    const float WaveSpacing = 10f;
-    const int BranchCount = 4;
+    const int WaveCount = 10;
+    const float WaveSpacing = BranchSpacing / (WaveCount - 1);
+    const int BranchCount = 5;
+    const float BranchSpacing = 360f / BranchCount;
+    const int BulletCount = 2;
+    const float BulletSpawnRadius = 0.1f;
+    const float SpawnRadiusModifier = 0.1f;
     const float BulletBaseSpeed = 4f;
-    const float BulletSpeedModifier = 0.04f;
-    const float BulletRotationDuration = 1f;
-    const float RotationDurationModifier = 0.02f;
+    const float BulletSpeedModifier = -0.2f;
+    const float BulletRotationSpeed = 90f;
+    const float BulletRotationDuration = 4f;
 
-    protected override float ShootingCooldown => 0.05f;
+    protected override float ShootingCooldown => 0.2f;
 
     protected override IEnumerator Shoot()
     {
@@ -22,13 +26,19 @@ public class LeoBulletSystem42 : EnemyShooter<EnemyBullet>
         {
             for (int ii = 0; ii < BranchCount; ii++)
             {
-                float z = (ii % 2 * 2 - 1) * (i * WaveSpacing) + r;
-                float s = BulletBaseSpeed + (i * BulletSpeedModifier);
-                float d = BulletRotationDuration + (i * RotationDurationModifier);
-                Vector3 pos = Vector3.zero;
+                for (int iii = 0; iii < BulletCount; iii++)
+                {
+                    int d = iii % 2 * 2 - 1;
+                    float z = (d * ((i * WaveSpacing) + (ii * BranchSpacing))) + r;
+                    float s = BulletBaseSpeed + (i * BulletSpeedModifier);
+                    Vector3 pos = (BulletSpawnRadius + (i * SpawnRadiusModifier)) * transform.up.RotateVectorBy(z);
 
-                var bullet = SpawnProjectile(2, z, pos);
-                bullet.StartCoroutine(bullet.LerpSpeed(s, 2f, d));
+                    bulletData.colour = bulletData.gradient.Evaluate(iii);
+
+                    var bullet = SpawnProjectile(2, z, pos);
+                    bullet.StartCoroutine(bullet.LerpSpeed(1f, s, 1f));
+                    bullet.StartCoroutine(bullet.RotateBy(d * BulletRotationSpeed, BulletRotationDuration));
+                }
             }
 
             yield return WaitForSeconds(ShootingCooldown);
