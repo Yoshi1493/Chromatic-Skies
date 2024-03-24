d3c5a6e589e4be7b671c133f7585b4a637a8e673
Yoshi1493 <nic.choy107@gmail.com>
2024-03-24 03:13:40 -0400

    Leo EB, SO updates
    updated AP5

--- Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBullet53.cs
+++ Assets/Scripts/Game/Projectiles/Enemy bullet systems/Leo/LeoBullet53.cs
@@ -6,6 +6,6 @@ public class LeoBullet53 : EnemyBullet
 
     protected override IEnumerator Move()
     {
-        yield return this.LerpSpeed(5f, 3f, 0.5f);
+        yield return this.LerpSpeed(5f, 2.5f, 0.5f);
     }
 }
