e15e1c70df94dad6d2df36fbf1b2feb12272352c
Yoshi1493 <nic.choy107@gmail.com>
2024-04-06 19:08:13 -0400

    Create PlaeyrLoseLife VFX graph + prefab
    updated ShipParticleController.cs to spawn PlayerLoseLife particle effect upon player losing life

--- Assets/Scripts/Game/Ship/ShipParticleController.cs
+++ Assets/Scripts/Game/Ship/ShipParticleController.cs
@@ -2,21 +2,37 @@ using UnityEngine;
 
 public class ShipParticleController : MonoBehaviour
 {
+    Player player;
     Enemy enemy;
 
+    [SerializeField] GameObject playerLoseLifeParticleEffect;
     [SerializeField] GameObject enemyDeathParticleEffect;
 
     void Awake()
     {
+        player = FindObjectOfType<Player>();
+        player.LoseLifeAction += OnPlayerLoseLife;
+
         enemy = FindObjectOfType<Enemy>();
         enemy.DeathAction += OnEnemyDeath;
     }
 
+    void OnPlayerLoseLife()
+    {
+        var vfx = Instantiate(playerLoseLifeParticleEffect, transform);
+        vfx.transform.position = player.transform.position;
+
+        var particleEffect = vfx.GetComponent<ParticleEffect>();
+        particleEffect.ParticleSystem.SetVector4("ParticleColour", player.shipData.UIColour.value);
+        particleEffect.enabled = true;
+    }
+
     void OnEnemyDeath()
     {
-        var vfx = Instantiate(enemyDeathParticleEffect, enemy.transform);
-        var particleEffect = vfx.GetComponent<ParticleEffect>();
+        var vfx = Instantiate(enemyDeathParticleEffect, transform);
+        vfx.transform.position = enemy.transform.position;
 
+        var particleEffect = vfx.GetComponent<ParticleEffect>();
         particleEffect.ParticleSystem.SetVector4("ParticleColour", enemy.shipData.UIColour.value);
         particleEffect.enabled = true;
     }
