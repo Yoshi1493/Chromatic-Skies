4543063eb63ec85e5ad5249c599f0be4d4d5f149
Yoshi1493 <nic.choy107@gmail.com>
2024-04-07 23:13:44 -0400

    Create + impl. ShipDeathLightController

---
+++ Assets/Scripts/Game/VFX/Background effects/ShipDeathLightController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using static CoroutineHelper;
+
+[RequireComponent(typeof(Light2D))]
+public class ShipDeathLightController : MonoBehaviour
+{
+    Light2D globalLight;
+    IEnumerator intensityCoroutine;
+    const float LightIntensityAnimationDuration = 3.5f;
+
+    [SerializeField] AnimationCurve lightIntensityInterpolation;
+
+    void Awake()
+    {
+        globalLight = GetComponent<Light2D>();
+
+        Enemy enemy = FindObjectOfType<Enemy>();
+        enemy.DeathAction += OnEnemyDie;
+    }
+
+    void OnEnemyDie()
+    {
+        if (intensityCoroutine != null)
+        {
+            StopCoroutine(intensityCoroutine);
+        }
+
+        intensityCoroutine = _OnEnemyDie();
+        StartCoroutine(intensityCoroutine);
+    }
+
+    IEnumerator _OnEnemyDie()
+    {
+        float currentLerpTime = 0f;
+
+        while (currentLerpTime < LightIntensityAnimationDuration)
+        {
+            float t = lightIntensityInterpolation.Evaluate(currentLerpTime / LightIntensityAnimationDuration);
+
+            globalLight.intensity = Mathf.LerpUnclamped(0f, 1f, t);
+
+            yield return EndOfFrame;
+            currentLerpTime += Time.deltaTime;
+        }
+    }
+}
