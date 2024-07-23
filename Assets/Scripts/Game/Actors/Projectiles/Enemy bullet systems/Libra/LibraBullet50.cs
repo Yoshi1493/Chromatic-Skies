d62b858abba6ef3bb156227829ecd3648142cc37
Yoshi1493 <nic.choy107@gmail.com>
2024-07-23 17:27:28 -0400

    Libra EB updates
    updated AP5

--- Assets/Scripts/Game/Actors/Projectiles/Enemy bullet systems/Libra/LibraBullet50.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Enemy bullet systems/Libra/LibraBullet50.cs
@@ -1,14 +1,18 @@
 using System.Collections;
-using UnityEngine;
 using static CoroutineHelper;
 
 public class LibraBullet50 : EnemyBullet
 {
+    protected override float MaxLifetime => 20f;
+
     protected override IEnumerator Move()
     {
         yield return WaitForSeconds(1f);
 
         float startSpeed = MoveSpeed;
         yield return this.LerpSpeed(startSpeed, 0f, 1f);
+
+        yield return WaitForSeconds(1f);
+        yield return this.LerpSpeed(0f, 2f, 5f);
     }
 }
--- Assets/Scripts/Game/Actors/Projectiles/Enemy bullet systems/Libra/LibraBullet51.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Enemy bullet systems/Libra/LibraBullet51.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 public class LibraBullet51 : EnemyBullet
 {
-    protected override float MaxLifetime => 8f;
+    protected override float MaxLifetime => 12f;
 
     protected override IEnumerator Move()
     {
         yield return this.LerpSpeed(3f, 0f, 1f);
 
         StartCoroutine(this.RotateBy(Random.Range(-30f, 30f), 3f));
-        yield return this.LerpSpeed(0f, Random.Range(2f, 3f), 1f);
+        yield return this.LerpSpeed(0f, Random.Range(1.5f, 2f), 1f);
     }
 }
