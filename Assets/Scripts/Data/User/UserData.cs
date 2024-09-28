07490284f2b07c853a1183b1194a9de1d2ca54d2
Yoshi1493 <nic.choy107@gmail.com>
2024-09-27 21:12:01 -0400

    Settings menu updates
    added background dim slider (in-game functionality not impl.'d); updated UserData accordingly

--- Assets/Scripts/Data/User/UserData.cs
+++ Assets/Scripts/Data/User/UserData.cs
@@ -5,10 +5,12 @@ public class UserData
 {
     public float MusicVolume;
     public float SoundVolume;
+    public float BackgroundDim;
 
     public UserData()
     {
         MusicVolume = 0.8f;
         SoundVolume = 0.8f;
+        BackgroundDim = 0.3f;
     }
 }
--- Assets/Scripts/Menu/Settings menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/Settings menu/SettingsMenu.cs
@@ -10,6 +10,7 @@ public class SettingsMenu : Menu, ISavable
 
     [SerializeField] Slider musicSlider;
     [SerializeField] Slider soundSlider;
+    [SerializeField] Slider backgroundDimSlider;
 
     void Update()
     {
@@ -29,18 +30,25 @@ public class SettingsMenu : Menu, ISavable
         tmp.text = soundSlider.value.ToString();
     }
 
+    public void OnChangeBackgroundDim(TextMeshProUGUI tmp)
+    {
+        tmp.text = backgroundDimSlider.value.ToString();
+    }
+
     #region Interface impl.
 
     void ISavable.LoadData(UserData data)
     {
         musicSlider.value = data.MusicVolume * 10f;
         soundSlider.value = data.SoundVolume * 10f;
+        backgroundDimSlider.value = data.BackgroundDim * 100f;
     }
 
     void ISavable.SaveData(UserData data)
     {
         data.MusicVolume = musicSlider.value * 0.1f;
         data.SoundVolume = soundSlider.value * 0.1f;
+        data.BackgroundDim = backgroundDimSlider.value * 0.01f;
     }
 
     #endregion
