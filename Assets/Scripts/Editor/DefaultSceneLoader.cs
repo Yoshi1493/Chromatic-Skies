b1c3c5e2d0d5ae5e43782ef5d5b2114dcd31e4b1
Yoshi1493 <nic.choy107@gmail.com>
2024-10-03 23:32:36 -0400

    Menu scene updates
    (temp) disabled Achievements menu + removed option from Main menu

--- Assets/Scripts/Editor/DefaultSceneLoader.cs
+++ Assets/Scripts/Editor/DefaultSceneLoader.cs
@@ -3,15 +3,15 @@ using UnityEditor;
 using UnityEngine;
 using UnityEditor.SceneManagement;
 
-[InitializeOnLoad]
-public class DefaultSceneLoader : MonoBehaviour
-{
-    static DefaultSceneLoader()
-    {
-        var pathOfFirstScene = EditorBuildSettings.scenes[0].path;
-        var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(pathOfFirstScene);
-        EditorSceneManager.playModeStartScene = sceneAsset;
-        Debug.Log(pathOfFirstScene + " was set as default play mode scene");
-    }
-}
+//[InitializeOnLoad]
+//public class DefaultSceneLoader : MonoBehaviour
+//{
+//    static DefaultSceneLoader()
+//    {
+//        var pathOfFirstScene = EditorBuildSettings.scenes[0].path;
+//        var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(pathOfFirstScene);
+//        EditorSceneManager.playModeStartScene = sceneAsset;
+//        Debug.Log(pathOfFirstScene + " was set as default play mode scene");
+//    }
+//}
 #endif
