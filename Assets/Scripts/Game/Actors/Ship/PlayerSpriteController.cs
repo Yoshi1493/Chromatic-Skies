e322c6dd0928a1c929d2f9062004587dc4be0a0a
Yoshi1493 <nic.choy107@gmail.com>
2024-10-19 04:17:02 -0400

    Update PlayerSpriteController.cs
    disabled ability to display hitbox while paused

--- Assets/Scripts/Game/Actors/Ship/PlayerSpriteController.cs
+++ Assets/Scripts/Game/Actors/Ship/PlayerSpriteController.cs
@@ -4,14 +4,21 @@ public class PlayerSpriteController : ShipSpriteController<Player>
 {
     [SerializeField] SpriteRenderer hitboxVisualizer;
 
+    PauseHandler pauseHandler;
+
     protected override void Awake()
     {
         base.Awake();
+
         GetComponent<PlayerMovement>().MovementSlowAction += OnMovementSlow;
+        pauseHandler = FindObjectOfType<PauseHandler>();
     }
 
     void OnMovementSlow(bool state)
     {
-        hitboxVisualizer.enabled = state;
+        if (!pauseHandler.IsPaused)
+        {
+            hitboxVisualizer.enabled = state;
+        }
     }
 }
