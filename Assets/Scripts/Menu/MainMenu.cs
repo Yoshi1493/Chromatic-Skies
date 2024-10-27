65b2765a6fe2b98f00776ab1a42936d18ecbefc7
Yoshi1493 <nic.choy107@gmail.com>
2024-10-27 18:50:18 -0400

    Black hole VFX update

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,3 +1,18 @@
+using UnityEngine;
+
 public class MainMenu : Menu
 {
+    [SerializeField] protected GameObject blackHoleEffect;
+
+    public override void Enable(GameObject newSelectedGameObject)
+    {
+        base.Enable(newSelectedGameObject);
+        blackHoleEffect.SetActive(true);
+    }
+
+    public override void Disable()
+    {
+        blackHoleEffect.SetActive(false);
+        base.Disable();
+    }
 }
