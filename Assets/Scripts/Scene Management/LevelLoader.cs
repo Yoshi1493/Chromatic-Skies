6dc52d1394f15f881ae36e974b31a51a01494e9e
Yoshi1493 <nic.choy107@gmail.com>
2024-11-01 23:10:28 -0400

    Lock application FPS to 60

--- Assets/Scripts/Scene Management/LevelLoader.cs
+++ Assets/Scripts/Scene Management/LevelLoader.cs
@@ -16,6 +16,7 @@ public class LevelLoader : MonoBehaviour
 
     void Start()
     {
+        Application.targetFrameRate = 60;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
