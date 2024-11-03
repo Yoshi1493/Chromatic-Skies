e2321c6365efea9a98db2ec1a4d069800bcd6132
Yoshi1493 <nic.choy107@gmail.com>
2024-11-03 02:57:01 -0500

    FPS display bugfix
    fix FPS counter not displaying the correct value - now uses a rolling average across 30 sampled frames

--- Assets/Scripts/Data/FPSDisplay.cs
+++ Assets/Scripts/Data/FPSDisplay.cs
@@ -1,11 +1,18 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class FPSDisplay : MonoBehaviour
 {
     CanvasGroup canvasGroup;
-    [SerializeField] TextMeshProUGUI fpsText;
 
+    const int cacheSize = 30;
+    float[] fpsSamples = new float[cacheSize];
+    int avgFps;
+    float currentAvg;
+
+    [SerializeField] TextMeshProUGUI fpsText;
     bool showFps = true;
 
     void Awake()
@@ -15,9 +22,22 @@ public class FPSDisplay : MonoBehaviour
 
     void Update()
     {
+        float currentFps = 1f / Time.unscaledDeltaTime;
+        fpsSamples[avgFps] = currentFps;
+
+        float avg = 0f;
+
+        foreach (var fps in fpsSamples)
+        {
+            avg += fps;
+        }
+
+        currentAvg = avg / cacheSize;
+        avgFps = (avgFps + 1) % cacheSize;
+
         if (Time.frameCount % 30 == 0)
         {
-            fpsText.text = $"{1 / Time.unscaledDeltaTime:F1} fps";
+            fpsText.text = $"{currentAvg:F1} fps";
         }
 
         if (Input.GetKeyDown(KeyCode.F))
