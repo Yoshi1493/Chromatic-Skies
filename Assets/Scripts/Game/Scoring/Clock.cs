91d373e4d05e8aaba4323a03e78f29a06b32e15a
Yoshi1493 <nic.choy107@gmail.com>
2024-11-05 17:39:29 -0500

    Clock implementation
    updated Game scene with clock UI

--- Assets/Scripts/Game/Scoring/Clock.cs
+++ Assets/Scripts/Game/Scoring/Clock.cs
@@ -1,18 +1,57 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using TMPro;
+using static CoroutineHelper;
 
 public class Clock : MonoBehaviour
 {
     IEnumerator clock;
-    public float CurrentTime { get; private set; }
+    IEnumerator blinkCoroutine;
 
+    public float CurrentTime { get; private set; }
     public bool IsPaused { get; private set; }
+
+    TextMeshProUGUI clockText;
+    const string StringFormat = "m':'ss'.'ff";
 
     Enemy enemy;
     PauseHandler pauseHandler;
 
-    IEnumerator CountUp()
+    void Awake()
     {
+        clockText = GetComponent<TextMeshProUGUI>();
+
+        pauseHandler = FindObjectOfType<PauseHandler>();
+        enemy = FindObjectOfType<Enemy>();
+    }
+
+    void Start()
+    {
+        pauseHandler.GamePauseAction += SetPaused;
+
+        enemy.StartAttackAction += RestartClock;
+        enemy.DeathAction += StopClock;
+    }
+
+    void Update()
+    {
+        clockText.text = TimeSpan.FromSeconds(CurrentTime).ToString(StringFormat);
+    }
+
+    #region Clock functions
+
+    IEnumerator Run(float delay = 0f)
+    {
+        if (delay > 0f)
+        {
+            IsPaused = true;
+            yield return Blink(delay);
+        }
+
+        CurrentTime = 0f;
+        IsPaused = false;
+
         while (true)
         {
             yield return null;
@@ -24,39 +63,53 @@ public class Clock : MonoBehaviour
         }
     }
 
-    public void StartClock(float startTime)
+    IEnumerator Blink(float blinkDuration)
     {
-        CurrentTime = startTime;
+        float currentTime = 0f;
 
-        clock = CountUp();
+        while (currentTime < blinkDuration)
+        {
+            float a = Mathf.Cos(currentTime * Mathf.PI * 2f) * 0.5f + 0.5f;
+            SetTextAlpha(a);
+
+            yield return null;
+            currentTime += Time.deltaTime;
+        }
+
+        SetTextAlpha(1f);
+    }
+
+    void StartClock()
+    {
+        clock = Run(4f);
         StartCoroutine(clock);
     }
 
-    public void StopClock()
+    void StopClock()
     {
-        StopCoroutine(clock);
+        if (clock != null)
+        {
+            StopCoroutine(clock);
+        }
     }
 
-    public void SetPaused(bool state)
+    void RestartClock(int _)
+    {
+        StopClock();
+        StartClock();
+    }
+
+    void SetPaused(bool state)
     {
         IsPaused = state;
     }
 
-    public void RestartClock()
+    #endregion
+ 
+    void SetTextAlpha(float alpha)
     {
-        StopClock();
-        StartClock(0f);
-    }
-
-    void Awake()
-    {
-        pauseHandler = FindObjectOfType<PauseHandler>();
-        enemy = FindObjectOfType<Enemy>();
-    }
-
-    void Start()
-    {
-        pauseHandler.GamePauseAction += SetPaused;
-        enemy.DeathAction += StopClock;
+        Color c = clockText.color;
+        c.a = alpha;
+        clockText.color = c;
     }
 }
