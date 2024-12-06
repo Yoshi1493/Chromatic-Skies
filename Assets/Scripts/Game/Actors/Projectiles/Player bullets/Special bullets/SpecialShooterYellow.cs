eddc196284d19eae381dfd57e55b4603c74c16fb
Yoshi1493 <nic.choy107@gmail.com>
2024-12-06 00:13:37 -0500

    Player special updates
    updated Yellow special + bullet prefab

--- Assets/Scripts/Game/Actors/Projectiles/Player bullets/Special bullets/SpecialShooterYellow.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Player bullets/Special bullets/SpecialShooterYellow.cs
@@ -1,11 +1,45 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using static CoroutineHelper;
 
 public class SpecialShooterYellow : PlayerSpecialShooter
 {
+    const int WaveCount = 3;
+    const float WaveSpacing = 5f;
+    const int BranchCount = 40;
+    const float BranchSpacing = 360f / BranchCount;
+    const float BulletBaseSpeed = 5f;
+    protected override float ShootingCooldown => 12f / 60;
+
+    List<SpecialBullet> bullets = new(WaveCount * BranchCount);
+
     protected override IEnumerator Shoot()
     {
-        yield return null;
+        bullets.Clear();
+
+        yield return base.Shoot();
+
+        for (int i = 0; i < WaveCount; i++)
+        {
+            for (int ii = 0; ii < BranchCount; ii++)
+            {
+                float z = (i * WaveSpacing) + (ii * BranchSpacing);
+                Vector3 pos = Vector3.zero;
+
+                var bullet = SpawnProjectile(1, z, pos) as SpecialBullet;
+                bullet.StartCoroutine(bullet.LerpSpeed(BulletBaseSpeed, 0f, 1f));
+                bullets.Add(bullet);
+            }
+
+            yield return WaitForSeconds(ShootingCooldown);
+        }
+
+        yield return WaitForSeconds(1f);
+
+        bullets.ForEach(b => b.Fire());
+
+        yield return WaitForSeconds(SpecialCooldown);
+        canShoot = true;
     }
 }
--- Assets/Scripts/Game/Actors/Projectiles/Player bullets/Special bullets/SpecialYellow.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Player bullets/Special bullets/SpecialYellow.cs
@@ -4,8 +4,24 @@ using static CoroutineHelper;
 
 public class SpecialYellow : SpecialBullet
 {
+    protected override int MaxCollisions => 8;
+    protected override int NumCollisions => Physics2D.OverlapBoxNonAlloc(transform.position, SpriteRenderer.size, transform.eulerAngles.z, collisionResults, CollisionMask);
+
     protected override IEnumerator Move()
     {
-        yield return null;
+        moveDirection *= -1;
+
+        yield return this.LerpSpeed(0f, 2.5f, 0.5f);
+        yield return WaitForSeconds(0.75f);
+
+        Destroy();
     }
+
+#if UNITY_EDITOR
+    protected override void OnDrawGizmos()
+    {
+        Gizmos.matrix = collider.transform.localToWorldMatrix;
+        Gizmos.DrawCube(transform.position, SpriteRenderer.size);
+    }
+#endif
 }
