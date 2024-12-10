6cf82afe9157434b9f9a2dec969c77fc62ac29d8
Yoshi1493 <nic.choy107@gmail.com>
2024-12-10 18:28:59 -0500

    Player special updates
    updated Green special

--- Assets/Scripts/Game/Actors/Ship/Players/PlayerGreen.cs
+++ Assets/Scripts/Game/Actors/Ship/Players/PlayerGreen.cs
@@ -1,11 +1,55 @@
 using System.Collections;
 using UnityEngine;
+using static CoroutineHelper;
 
 public class PlayerGreen : Player
 {
+    [Space]
+
+    [SerializeField] FloatObject originalShootingSpeed;
+    [SerializeField] FloatObject rapidfireShootingSpeed;
+
+    IEnumerator rapidfireCoroutine;
+    IEnumerator afterimageCoroutine;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        shipData.ShootingSpeed.Variable = originalShootingSpeed;
+    }
+
     //to-do
     protected override void OnSpecialActivated()
     {
-        return;
+        if (rapidfireCoroutine != null)
+        {
+            StopCoroutine(rapidfireCoroutine);
+        }
+
+        if (afterimageCoroutine != null)
+        {
+            StopCoroutine(afterimageCoroutine);
+        }
+
+        rapidfireCoroutine = IncreaseShootingSpeed();
+        afterimageCoroutine = DisplayAfterimageTrail();
+
+        StartCoroutine(rapidfireCoroutine);
+        StartCoroutine(afterimageCoroutine);
+    }
+
+    IEnumerator IncreaseShootingSpeed()
+    {
+        shipData.ShootingSpeed.Variable = rapidfireShootingSpeed;
+
+        yield return WaitForSeconds(5f);
+
+        shipData.ShootingSpeed.Variable = originalShootingSpeed;
+    }
+
+    IEnumerator DisplayAfterimageTrail()
+    {
+        //SpriteRenderer
+        yield return WaitForSeconds(4.5f);
     }
 }
