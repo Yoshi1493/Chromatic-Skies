db7060f0ca3271d9d7741e8b59d8b540ae5912ed
Yoshi1493 <nic.choy107@gmail.com>
2024-12-12 01:35:26 -0500

    Player special updates
    updated Green special

--- Assets/Scripts/Game/Actors/Ship/Players/PlayerGreenVFXController.cs
+++ Assets/Scripts/Game/Actors/Ship/Players/PlayerGreenVFXController.cs
@@ -7,13 +7,14 @@ public class PlayerGreenVFXController : PlayerParticleController
     [Space]
 
     [SerializeField] PlayerSpecialShooter specialShooter;
-    [SerializeField] SpriteRenderer afterimageSheet;
     IEnumerator afterimageCoroutine;
 
     protected override void Awake()
     {
         base.Awake();
+
         specialShooter.SpecialAction += OnSpecialActivated;
+        specialVFX.SetVector4("ParticleColour", parentShip.shipData.UIColour.value);
     }
 
     void OnSpecialActivated()
@@ -29,15 +30,25 @@ public class PlayerGreenVFXController : PlayerParticleController
 
     IEnumerator DisplayAfterimageTrail()
     {
+        transform.parent = null;
+
         float currentTime = 0f;
-        float totalDisplayTime = 4.5f;
-        float displayInterval = 0.1f;
+        float totalDisplayTime = 4f;
+
+        PlayVisualEffect(specialVFX);
+        specialVFX.SetVector2("OriginalPosition", parentShip.transform.position);
 
         while (currentTime < totalDisplayTime)
         {
-            //to-do: impl.
-            yield return WaitForSeconds(displayInterval);
-            currentTime += displayInterval;
+            specialVFX.SetVector2("SpawnPosition", parentShip.transform.position);
+
+            yield return null;
+            currentTime += Time.deltaTime;
         }
+
+        yield return WaitUntil(() => specialVFX.aliveParticleCount == 0);
+        transform.parent = parentShip.transform;
+
+        StopVisualEffect(specialVFX);
     }
 }
