7b834c1d324acacb4c7b22abbb8dc69b8d48ea5b
Yoshi1493 <nic.choy107@gmail.com>
2024-12-27 22:32:38 -0500

    Update Controls menu
    separated content into 2 columns

--- Assets/Scripts/Menu/Controls menu/ControlsMenu.cs
+++ Assets/Scripts/Menu/Controls menu/ControlsMenu.cs
@@ -1,9 +1,27 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class ControlsMenu : Menu
 {
+    [SerializeField] GameObject menuContentL;
+    [SerializeField] GameObject menuContentR;
+    Button[] menuButtonsL;
+    Button[] menuButtonsR;
+
     [SerializeField] Button backButton;
+
+    GameObject lastSelectedNonBackButton;
+    bool updatedBackButtonNavigation;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        menuButtonsL = menuContentL.GetComponentsInChildren<Button>();
+        menuButtonsR = menuContentR.GetComponentsInChildren<Button>();
+    }
 
     void Update()
     {
@@ -11,5 +29,35 @@ public class ControlsMenu : Menu
         {
             backButton.OnPointerClick(eventData);
         }
+
+        if (EventSystem.current.currentSelectedGameObject != backButton.gameObject)
+        {
+            lastSelectedNonBackButton = EventSystem.current.currentSelectedGameObject;
+            updatedBackButtonNavigation = false;
+        }
+        else
+        {
+            if (!updatedBackButtonNavigation)
+            {
+                Navigation nav = new();
+                nav.mode = Navigation.Mode.Explicit;
+
+                if (Array.Exists(menuButtonsL, b => b == lastSelectedNonBackButton.GetComponent<Button>()))
+                {
+                    nav.selectOnDown = menuButtonsL[0];
+                    nav.selectOnUp = menuButtonsL[^1];
+                }
+                if (Array.Exists(menuButtonsR, b => b == lastSelectedNonBackButton.GetComponent<Button>()))
+                {
+                    nav.selectOnDown = menuButtonsR[0];
+                    nav.selectOnUp = menuButtonsR[^1];
+                }
+
+                backButton.navigation = nav;
+
+                updatedBackButtonNavigation = true;
+            }
+        }
     }
+
 }
