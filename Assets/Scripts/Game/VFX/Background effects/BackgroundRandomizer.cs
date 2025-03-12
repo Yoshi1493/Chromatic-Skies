75669c6ef4b65866e67d70c8c3552082694fd8a7
Yoshi1493 <nic.choy107@gmail.com>
2025-03-12 19:29:12 -0400

    Update Main scene
    created BackgroundRandomizer to randomly select colour palette and automatically adjust size of background tiles

---
+++ Assets/Scripts/Game/VFX/Background effects/BackgroundRandomizer.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using static MathHelper;
+
+public class BackgroundRandomizer : MonoBehaviour
+{
+    [SerializeField] Gradient[] colourPalettes;
+    [SerializeField] SpriteRenderer[] backgroundTiles;
+
+    void Awake()
+    {
+        Camera mainCam = Camera.main;
+        float screenHalfHeight = mainCam.orthographicSize;
+        float screenHalfWidth = screenHalfHeight * mainCam.aspect;
+
+        int gradientIndex = Random.Range(0, colourPalettes.Length);
+        int gradientLen = colourPalettes[gradientIndex].colorKeys.Length;
+
+        //set active, set colour
+        for (int i = 0; i < backgroundTiles.Length; i++)
+        {
+            if (i < gradientLen)
+            {
+                backgroundTiles[i].gameObject.SetActive(true);
+                backgroundTiles[i].color = colourPalettes[gradientIndex].colorKeys[i].color;
+            }
+            else
+            {
+                backgroundTiles[i].gameObject.SetActive(false);
+            }
+        }
+
+        //get random rotation first
+        float rotZ = Random.Range(10f, 25f) * PositiveOrNegativeOne;
+
+        //set transform
+        for (int i = 0; i < backgroundTiles.Length; i++)
+        {
+            if (!backgroundTiles[i].gameObject.activeSelf) break;
+
+            float posX = Mathf.Lerp(-screenHalfWidth, screenHalfWidth, i / (gradientLen - 1f));
+            float scaleX = screenHalfWidth * 2f / (gradientLen - 1);
+            float scaleY = ((screenHalfHeight * 2) / Mathf.Sin((90f - Mathf.Abs(rotZ)) * Mathf.Deg2Rad)) + (scaleX / Mathf.Tan((90f - Mathf.Abs(rotZ)) * Mathf.Deg2Rad));            
+
+            backgroundTiles[i].transform.position = posX * Vector3.right;
+            backgroundTiles[i].transform.eulerAngles = rotZ * Vector3.forward;
+            backgroundTiles[i].transform.localScale = new(scaleX, scaleY);
+        }
+    }
+}
