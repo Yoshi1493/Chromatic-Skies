50f8a419b6f63071c1b714cecc378d00245329ef
Yoshi1493 <nic.choy107@gmail.com>
2025-03-24 15:45:12 -0400

    Implement Collectible collision with Player
    updated ScoreObject accordingly

--- Assets/Scripts/Game/Actors/Collectibles/Collectible.cs
+++ Assets/Scripts/Game/Actors/Collectibles/Collectible.cs
@@ -1,10 +1,83 @@
+using UnityEngine;
+
 public abstract class Collectible : Actor
 {
+    int CollisionMask => 1 << LayerMask.NameToLayer("Player");
+    Collider2D[] collisionResults = new Collider2D[1];
+    float hitboxSize;
+
+    [HideInInspector] public Vector3 moveDirection;
+    public float MoveSpeed { get; protected set; }
+
     Player player;
+    bool foundPlayer;
 
     protected override void Awake()
     {
         base.Awake();
         player = FindObjectOfType<Player>();
     }
+
+    protected virtual void OnEnable()
+    {
+        moveDirection = Vector3.down;
+        MoveSpeed = 2f;
+
+        foundPlayer = false;
+    }
+
+    void Start()
+    {
+        hitboxSize = Mathf.Min(SpriteRenderer.size.x, SpriteRenderer.size.y) / 2f;
+    }
+
+    void Update()
+    {
+        transform.Translate(Time.deltaTime * MoveSpeed * moveDirection.normalized, Space.World);
+
+        if (!foundPlayer)
+        {
+            CheckCollisionWithPlayer();
+        }
+        else
+        {
+            HomeInOnPlayer();
+        }
+    }
+
+    void CheckCollisionWithPlayer()
+    {
+        var collisions = Physics2D.OverlapCircleNonAlloc(transform.position, hitboxSize, collisionResults, CollisionMask);
+
+        if (collisions > 0)
+        {
+            foundPlayer = true;
+        }
+    }
+
+    void HomeInOnPlayer()
+    {
+        Vector3 diff = player.transform.position - transform.position;
+        moveDirection = diff;
+        MoveSpeed = 10f;
+
+        if (Vector3.SqrMagnitude(diff) <= 0.1f)
+        {
+            Destroy();
+        }
+    }
+
+    protected virtual void Destroy()
+    {
+        moveDirection = Vector3.zero;
+        MoveSpeed = 0f;
+    }
+
+#if UNITY_EDITOR
+    protected virtual void OnDrawGizmos()
+    {
+        if (UnityEditor.EditorApplication.isPlaying)
+            Gizmos.DrawSphere(transform.position, hitboxSize);
+    }
+#endif
 }
--- Assets/Scripts/Game/Actors/Collectibles/ScoreObject.cs
+++ Assets/Scripts/Game/Actors/Collectibles/ScoreObject.cs
@@ -1,4 +1,8 @@
 public class ScoreObject : Collectible
 {
-
+    protected override void Destroy()
+    {
+        base.Destroy();
+        CollectibleObjectPool.Instance.ReturnToPool(this, (int)CollectibleType.Score);
+    }
 }
