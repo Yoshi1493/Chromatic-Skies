55a39154385755b6d5422e526700b79fba5d75eb
Yoshi1493 <nic.choy107@gmail.com>
2025-03-31 13:42:34 -0400

    Impl. mob wave spawner

--- Assets/Scripts/Game/Actors/Spawners/EnemySpawner.cs
+++ Assets/Scripts/Game/Actors/Spawners/EnemySpawner.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using static CoroutineHelper;
 
 public class EnemySpawner : MonoBehaviour
 {
+    new Transform transform;
+
     [SerializeField] TextAsset spawnFile;
     [SerializeField] Enemy[] enemyPrefabs;
 
@@ -13,10 +17,11 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator enemySpawnCoroutine;
     public event Action BossSpawnAction;
+    List<Enemy> enemies = new();
 
     void Awake()
     {
-        //to-do: parse spawn file
+        transform = GetComponent<Transform>();
 
         if (enemySpawnCoroutine != null)
         {
@@ -26,12 +31,59 @@ public class EnemySpawner : MonoBehaviour
         StartCoroutine(enemySpawnCoroutine);
     }
 
-    //to-do: pre-spawn all enemies
     IEnumerator SpawnEnemies()
     {
+        //parse spawn file
+        var splitFile = spawnFile.text.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        splitFile = splitFile.Skip(0).ToArray();
+
+        List<int> enemyIndexes = new();
+        List<float> xPositions = new();
+        List<float> yPositions = new();
+        List<float> spawnDelays = new();
+
+        foreach (var line in splitFile)
+        {
+            if (char.IsDigit(line[0]))
+            {
+                var splitLine = line.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                enemyIndexes.Add(int.Parse(splitLine[0]));
+                xPositions.Add(float.Parse(splitLine[1]));
+                yPositions.Add(float.Parse(splitLine[2]));
+                spawnDelays.Add(float.Parse(splitLine[3]));
+            }
+        }
+
+        //pre-spawn all enemies
+        for (int i = 0; i < enemyIndexes.Count; i++)
+        {
+            var enemy = Instantiate(enemyPrefabs[enemyIndexes[i]], transform);
+            Vector2 pos = new(xPositions[i], yPositions[i]);
+            enemy.transform.SetPositionAndRotation(pos, transform.rotation);
+
+            enemy.enabled = false;
+            enemy.gameObject.SetActive(false);
+
+            enemies.Add(enemy);
+        }
+
+        //pre-spawn boss
         var boss = Instantiate(bossPrefabs[selectedBossIndex.value], transform.position, transform.rotation);
         yield return null;
         boss.gameObject.SetActive(false);
+
+        //activate enemies
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if (spawnDelays[i] > 0f)
+            {
+                yield return WaitForSeconds(spawnDelays[i]);
+            }
+
+            enemies[i].gameObject.SetActive(true);
+            enemies[i].enabled = true;
+        }
 
         yield return WaitForSeconds(2f);
         boss.gameObject.SetActive(true);
