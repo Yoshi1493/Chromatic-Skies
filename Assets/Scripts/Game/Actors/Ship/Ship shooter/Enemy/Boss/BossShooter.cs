3127361412573547418ad595ccb7f3c38c102b73
Yoshi1493 <nic.choy107@gmail.com>
2025-04-07 15:45:50 -0400

    Create EnemyShooter
    moved some methods from BossShooter to EnemyShooter
    updated BossShooter to inherit from EnemyShooter

--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Boss/BossShooter.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Boss/BossShooter.cs
@@ -25,46 +25,10 @@ public abstract class BossShooter<TProjectile> : EnemyShooter<TProjectile>, IBos
 
     #endregion
 
-    Player playerShip;
-    protected Vector3 PlayerPosition => playerShip.transform.position;
-
-    protected float screenHalfHeight;
-    protected float screenHalfWidth;
-
-    [Space]
-
-    [SerializeField] protected ProjectileObject bulletData;
-
-    protected override void Awake()
-    {
-        base.Awake();
-
-        //find player
-        playerShip = FindObjectOfType<Player>();
-
-        //determine screen dimensions
-        Camera mainCam = Camera.main;
-        screenHalfHeight = mainCam.orthographicSize;
-        screenHalfWidth = screenHalfHeight * mainCam.aspect;
-    }
-
     protected override void Start()
     {
         base.Start();
         playerShip.LoseLifeAction += OnPlayerLoseLife;
-    }
-
-    protected override void OnEnable()
-    {
-        base.OnEnable();
-
-        if (shootCoroutine != null)
-        {
-            StopCoroutine(shootCoroutine);
-        }
-
-        shootCoroutine = Shoot();
-        StartCoroutine(shootCoroutine);
     }
 
     protected override IEnumerator Shoot()
@@ -88,12 +52,6 @@ public abstract class BossShooter<TProjectile> : EnemyShooter<TProjectile>, IBos
                 Debug.LogError("Error: Subsystem is already enabled.");
             }
         }
-    }
-
-    protected override void OnLoseLife()
-    {
-        StopAllCoroutines();
-        base.OnLoseLife();
     }
 
     void OnPlayerLoseLife()
--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Common/EnemyShooter.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Common/EnemyShooter.cs
@@ -4,5 +4,45 @@ using UnityEngine;
 public abstract class EnemyShooter<TProjectile> : Shooter<TProjectile>
     where TProjectile : Projectile
 {
+    protected Player playerShip;
+    protected Vector3 PlayerPosition => playerShip.transform.position;
 
+    protected float screenHalfHeight;
+    protected float screenHalfWidth;
+
+    [Space]
+
+    [SerializeField] protected ProjectileObject bulletData;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        //find player
+        playerShip = FindObjectOfType<Player>();
+
+        //determine screen dimensions
+        Camera mainCam = Camera.main;
+        screenHalfHeight = mainCam.orthographicSize;
+        screenHalfWidth = screenHalfHeight * mainCam.aspect;
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        if (shootCoroutine != null)
+        {
+            StopCoroutine(shootCoroutine);
+        }
+
+        shootCoroutine = Shoot();
+        StartCoroutine(shootCoroutine);
+    }
+
+    protected override void OnLoseLife()
+    {
+        StopAllCoroutines();
+        base.OnLoseLife();
+    }
 }
