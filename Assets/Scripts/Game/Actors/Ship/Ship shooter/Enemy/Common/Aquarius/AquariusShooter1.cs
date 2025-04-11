adaeda0f1d8800b04559ef79f00295eef4e5bc42
Yoshi1493 <nic.choy107@gmail.com>
2025-04-10 22:26:21 -0400

    Update AquariusShooter1.cs

--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Common/Aquarius/AquariusShooter1.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Common/Aquarius/AquariusShooter1.cs
@@ -4,22 +4,21 @@ using static CoroutineHelper;
 
 public class AquariusShooter1 : EnemyShooter<EnemyBullet>
 {
-    const int WaveCount = 3;
     const int BulletCount = 3;
+
+    protected override float ShootingCooldown => 0.2f;
 
     protected override IEnumerator Shoot()
     {
-        yield return WaitForSeconds(2f);
+        yield return WaitForSeconds(2.5f);
 
-        for (int i = 0; i < WaveCount; i++)
+        float z = PlayerPosition.GetRotationDifference(transform.position);
+        Vector3 pos = Vector3.zero;
+
+        for (int i = 0; i < BulletCount; i++)
         {
-            float z = PlayerPosition.GetRotationDifference(transform.position);
-            Vector3 pos = Vector3.zero;
-
-            for (int ii = 0; ii < BulletCount; ii++)
-            {
-                SpawnProjectile(0, z, pos).Fire();
-            }
+            SpawnProjectile(0, z, pos).Fire();
+            yield return WaitForSeconds(ShootingCooldown);
         }
     }
 }
