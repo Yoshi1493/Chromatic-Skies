faf314d96566c4b0b5d606edc7de3c5d26aef27b
Yoshi1493 <nic.choy107@gmail.com>
2025-04-10 22:07:01 -0400

    Create AquariusShooter1.cs
    added to prefab

--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Common/Aquarius/AquariusShooter1.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Common/Aquarius/AquariusShooter1.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using UnityEngine;
+using static CoroutineHelper;
 
 public class AquariusShooter1 : EnemyShooter<EnemyBullet>
 {
+    const int WaveCount = 3;
+    const int BulletCount = 3;
+
     protected override IEnumerator Shoot()
     {
-        yield return null;
+        yield return WaitForSeconds(2f);
+
+        for (int i = 0; i < WaveCount; i++)
+        {
+            float z = PlayerPosition.GetRotationDifference(transform.position);
+            Vector3 pos = Vector3.zero;
+
+            for (int ii = 0; ii < BulletCount; ii++)
+            {
+                SpawnProjectile(0, z, pos).Fire();
+            }
+        }
     }
 }
