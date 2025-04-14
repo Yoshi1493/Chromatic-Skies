29941c11211304bc2548685722a145052e66e6d6
Yoshi1493 <nic.choy107@gmail.com>
2025-04-14 11:32:28 -0400

    Update EnemySpawner.cs
    rearranged spawn order in SpawnEnemies method, to spawn boss first before checking for enemies (better for dev purposes, no impact to gameplay)
    added condition to check if spawn text file was null before spawning enemies (for dev purposes once again)
    renamed `spawnFile` to `spawnTextFile`; updated Game scene

--- Assets/Scripts/Game/Actors/Spawners/EnemySpawner.cs
+++ Assets/Scripts/Game/Actors/Spawners/EnemySpawner.cs
@@ -9,7 +9,7 @@ public class EnemySpawner : MonoBehaviour
 {
     new Transform transform;
 
-    [SerializeField] TextAsset spawnFile;
+    [SerializeField] TextAsset spawnTextFile;
     [SerializeField] Enemy[] enemyPrefabs;
     [SerializeField] Boss bossPrefab;
 
@@ -31,69 +31,73 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnEnemies()
     {
-        //parse spawn file
-        var splitFile = spawnFile.text.Split('\n', StringSplitOptions.RemoveEmptyEntries);
-        splitFile = splitFile.Where(i => char.IsDigit(i[0])).ToArray();
-
-        List<float> spawnTimes = new();
-        List<int> enemyIndexes = new();
-        List<float> xPositions = new();
-        List<float> yPositions = new();
-
-        foreach (var line in splitFile)
-        {
-            if (char.IsDigit(line[0]))
-            {
-                var splitLine = line.Split(", ", StringSplitOptions.RemoveEmptyEntries);
-
-                spawnTimes.Add(float.Parse(splitLine[0]));
-                enemyIndexes.Add(int.Parse(splitLine[1]));
-                xPositions.Add(float.Parse(splitLine[2]));
-                yPositions.Add(float.Parse(splitLine[3]));
-            }
-        }
-
-        if (enemyPrefabs.Length > 0)
-        {
-            //pre-spawn all enemies
-            for (int i = 0; i < enemyIndexes.Count; i++)
-            {
-                //make sure enemy index is within spawn array
-                if (enemyIndexes[i] < enemyPrefabs.Length)
-                {
-                    var enemy = Instantiate(enemyPrefabs[enemyIndexes[i]], transform);
-                    Vector2 pos = new(xPositions[i], yPositions[i]);
-                    enemy.transform.SetPositionAndRotation(pos, transform.rotation);
-
-                    enemy.enabled = false;
-                    enemy.gameObject.SetActive(false);
-
-                    enemies.Add(enemy);
-                }
-            }
-        }
-
-        //pre-spawn boss
+        //pre-spawn boss first
         var boss = Instantiate(bossPrefab, transform.position, transform.rotation);
         yield return null;
 
         boss.gameObject.SetActive(false);
 
-        //activate all enemies based on listed spawn time
-        for (int i = 0; i < enemies.Count; i++)
+        if (spawnTextFile != null)
         {
-            float delay = i > 0 ? spawnTimes[i] - spawnTimes[i - 1] : spawnTimes[0];
+            //parse spawn file
+            var splitFile = spawnTextFile.text.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            splitFile = splitFile.Where(i => char.IsDigit(i[0])).ToArray();
 
-            if (delay > 0f)
+            List<float> spawnTimes = new();
+            List<int> enemyIndexes = new();
+            List<float> xPositions = new();
+            List<float> yPositions = new();
+
+            foreach (var line in splitFile)
             {
-                yield return WaitForSeconds(delay);
+                if (char.IsDigit(line[0]))
+                {
+                    var splitLine = line.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                    spawnTimes.Add(float.Parse(splitLine[0]));
+                    enemyIndexes.Add(int.Parse(splitLine[1]));
+                    xPositions.Add(float.Parse(splitLine[2]));
+                    yPositions.Add(float.Parse(splitLine[3]));
+                }
             }
 
-            enemies[i].gameObject.SetActive(true);
-            enemies[i].enabled = true;
+            if (enemyPrefabs.Length > 0)
+            {
+                //pre-spawn all enemies
+                for (int i = 0; i < enemyIndexes.Count; i++)
+                {
+                    //make sure enemy index is within spawn array
+                    if (enemyIndexes[i] < enemyPrefabs.Length)
+                    {
+                        var enemy = Instantiate(enemyPrefabs[enemyIndexes[i]], transform);
+                        Vector2 pos = new(xPositions[i], yPositions[i]);
+                        enemy.transform.SetPositionAndRotation(pos, transform.rotation);
+
+                        enemy.enabled = false;
+                        enemy.gameObject.SetActive(false);
+
+                        enemies.Add(enemy);
+                    }
+                }
+            }
+
+            //activate all enemies based on listed spawn time
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                float delay = i > 0 ? spawnTimes[i] - spawnTimes[i - 1] : spawnTimes[0];
+
+                if (delay > 0f)
+                {
+                    yield return WaitForSeconds(delay);
+                }
+
+                enemies[i].gameObject.SetActive(true);
+                enemies[i].enabled = true;
+            }
+
+            yield return WaitUntil(() => transform.childCount == 0);
         }
 
-        yield return WaitUntil(() => transform.childCount == 0);
         yield return WaitForSeconds(3f);
 
         //activate boss
