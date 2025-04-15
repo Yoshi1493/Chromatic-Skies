cf1e82e9cc63f3f55d4a402fa6085d03ae7fe6c8
Yoshi1493 <nic.choy107@gmail.com>
2025-04-15 16:28:28 -0400

    Update enemy spawn instructions

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Common/MoveRelative.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Common/MoveRelative.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 
 public class MoveRelative : CommonEnemyMovement
 {
-    [SerializeField] float moveDuration;
     [SerializeField] float moveSpeed;
     [SerializeField] Vector2 normalizedDirection;
 
     protected override IEnumerator Move()
     {
-        yield return this.MoveRelative(normalizedDirection, moveSpeed, moveDuration, delay);
+        yield return this.MoveRelative(normalizedDirection, moveSpeed, duration, delay);
     }
 }
