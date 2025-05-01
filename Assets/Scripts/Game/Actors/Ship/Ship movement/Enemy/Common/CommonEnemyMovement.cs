09b33d64accf77870b203851d72d0a7137edfecb
Yoshi1493 <nic.choy107@gmail.com>
2025-05-01 16:01:00 -0400

    Update CommonEnemyMovement.cs
    added RequireComponent attribute to class

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Common/CommonEnemyMovement.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Common/CommonEnemyMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public abstract class CommonEnemyMovement : ShipMovement<Enemy>
 {
     [SerializeField] protected float delay;
