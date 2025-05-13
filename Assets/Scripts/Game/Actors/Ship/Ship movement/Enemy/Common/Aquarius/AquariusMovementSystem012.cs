21c80c0d05dc0b13ca3ea0399e51f5303d40f9e8
Yoshi1493 <nic.choy107@gmail.com>
2025-05-13 13:49:17 -0400

    Update Aquarius CE SO, MS, prefabs

---
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Common/Aquarius/AquariusMovementSystem012.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using UnityEngine;
+using static CoroutineHelper;
+
+public class AquariusMovementSystem012 : CommonEnemyMovement
+{
+    protected override IEnumerator Move()
+    {
+        yield return null;
+    }
+}
---
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Common/Aquarius/AquariusMovementSystem05.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using UnityEngine;
+using static CoroutineHelper;
+
+public class AquariusMovementSystem05 : CommonEnemyMovement
+{
+    protected override IEnumerator Move()
+    {
+        yield return null;
+    }
+}
