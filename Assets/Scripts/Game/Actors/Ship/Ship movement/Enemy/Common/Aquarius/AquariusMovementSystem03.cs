506aa569b54bb780ba70d8ff585d9485b957f470
Yoshi1493 <nic.choy107@gmail.com>
2025-05-15 18:59:14 -0400

    Update Aquarius CE spawn file

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Common/Aquarius/AquariusMovementSystem03.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Common/Aquarius/AquariusMovementSystem03.cs
@@ -4,8 +4,19 @@ using static CoroutineHelper;
 
 public class AquariusMovementSystem03 : CommonEnemyMovement
 {
+    [SerializeField] Vector2[] rotationPoints;
+
     protected override IEnumerator Move()
     {
-        yield return null;
+        float dist = Vector2.Distance(transform.position, rotationPoints[0]);
+        float theta = Mathf.Asin(2f / dist) * Mathf.Rad2Deg * 2;
+
+        for (int i = 0; i < rotationPoints.Length; i++)
+        {
+            yield return parentShip.TranslateAround(rotationPoints[i], (i % 2 * 2 - 1) * theta, 1.5f);
+        }
+
+        yield return WaitForSeconds(1f);
+        yield return LeaveScene();
     }
 }
