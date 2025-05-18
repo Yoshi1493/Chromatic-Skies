7f77ef092c37c50ad077b57e4785e9301672a49a
Yoshi1493 <nic.choy107@gmail.com>
2025-05-18 00:44:29 -0400

    Update Aquarius SO, MS, prefabs

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Common/Aquarius/AquariusMovementSystem04.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Common/Aquarius/AquariusMovementSystem04.cs
@@ -6,6 +6,11 @@ public class AquariusMovementSystem04 : CommonEnemyMovement
 {
     protected override IEnumerator Move()
     {
-        yield return null;
+        yield return parentShip.MoveRelative(Vector3.down, 2f, Random.Range(1.5f, 2f));
+        yield return WaitForSeconds(1f);
+        yield return parentShip.MoveRelative(Vector3.down, 3f, 3f);
+
+        yield return WaitForSeconds(1f);
+        yield return LeaveScene();
     }
 }
