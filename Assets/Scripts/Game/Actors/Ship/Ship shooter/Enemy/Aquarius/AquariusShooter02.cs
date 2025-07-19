7173e6bfa94312c2857cae34dfd7c7b03e3ebf93
Yoshi1493 <nic.choy107@gmail.com>
2025-07-19 00:17:16 -0400

    Update Aquarius CE data
    updated EBS, SO, prefabs

--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Aquarius/AquariusShooter02.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Aquarius/AquariusShooter02.cs
@@ -1,11 +1,44 @@
 using System.Collections;
 using UnityEngine;
+using static CoroutineHelper;
 
 public class AquariusShooter02 : EnemyShooter<EnemyBullet>
 {
+    const int RepeatCount = 2;
+    const int WaveCount = 8;
+    const float WaveSpacing = 2f;
+    const int BranchCount = 15;
+    const float BranchSpacing = 360f / BranchCount;
+
     protected override IEnumerator Shoot()
     {
-        yield break;
+        yield return WaitForSeconds(1.5f);
 
+        while (enabled)
+        {
+            for (int i = 0; i < RepeatCount; i++)
+            {
+                float r = PlayerPosition.GetRotationDifference(transform.position);
+
+                for (int ii = 0; ii < WaveCount; ii++)
+                {
+                    for (int iii = 0; iii < BranchCount; iii++)
+                    {
+                        float z = (i % 2 * 2 - 1) * ((ii * WaveSpacing) + (iii * BranchSpacing)) + r;
+                        Vector3 pos = Vector3.zero;
+
+                        bulletData.colour = bulletData.gradient.Evaluate(i);
+
+                        SpawnProjectile(2, z, pos).Fire();
+                    }
+
+                    yield return WaitForSeconds(ShootingCooldown);
+                }
+
+                yield return WaitForSeconds(1f);
+            }
+
+            yield return WaitForSeconds(2f);
+        }
     }
 }
