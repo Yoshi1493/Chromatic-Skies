5ca93e09557eb20469b7aa867a5f2460a50fe64e
Yoshi1493 <nic.choy107@gmail.com>
2025-07-20 17:39:31 -0400

    Update AquariusShooter04.cs

--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Aquarius/AquariusShooter04.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Aquarius/AquariusShooter04.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using UnityEngine;
+using static CoroutineHelper;
 
 public class AquariusShooter04 : EnemyShooter<EnemyBullet>
 {
+    const int WaveCount = 3;
+    const int BulletCount = 3;
+    const float BulletSpacing = 15f;
+
     protected override IEnumerator Shoot()
     {
-        yield break;
+        yield return WaitForSeconds(2.5f);
+
+        float r = PlayerPosition.GetRotationDifference(transform.position);
+
+        for (int i = 0; i < WaveCount; i++)
+        {
+            for (int ii = 0; ii < BulletCount; ii++)
+            {
+                float z = ((ii - (BulletCount - 1) / 2) * BulletSpacing) + r;
+                Vector3 pos = Vector3.zero;
+
+                SpawnProjectile(4, z, pos).Fire();
+            }
+
+            yield return WaitForSeconds(ShootingCooldown);
+        }
 
     }
 }
