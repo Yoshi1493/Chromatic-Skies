a95ce48017475e332ac9b9b73a627ea30841f161
Yoshi1493 <nic.choy107@gmail.com>
2025-07-21 21:17:10 -0400

    Update AquariusShooter02.cs

--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Aquarius/AquariusShooter02.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Aquarius/AquariusShooter02.cs
@@ -6,7 +6,8 @@ public class AquariusShooter02 : EnemyShooter<EnemyBullet>
 {
     const int RepeatCount = 2;
     const float RepeatSpacing = BranchSpacing / 2f;
-    const int WaveCount = 3;
+    const int WaveCount = 5;
+    const float WaveSpacing = 4f;
     const int BranchCount = 15;
     const float BranchSpacing = 360f / BranchCount;
 
@@ -26,7 +27,7 @@ public class AquariusShooter02 : EnemyShooter<EnemyBullet>
                 {
                     for (int iii = 0; iii < BranchCount; iii++)
                     {
-                        float z = (i * RepeatSpacing) + (iii * BranchSpacing) + r;
+                        float z = (i * RepeatSpacing) + ((i % 2 * 2 - 1) * (ii * WaveSpacing)) + (iii * BranchSpacing) + r;
                         Vector3 pos = Vector3.zero;
 
                         bulletData.colour = bulletData.gradient.Evaluate(i);
