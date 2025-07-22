8e4f904a7609705ba2bef7dfb5387f42e4fce403
Yoshi1493 <nic.choy107@gmail.com>
2025-07-22 17:49:28 -0400

    Update AquariusShooter01.cs

--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Aquarius/AquariusShooter01.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Aquarius/AquariusShooter01.cs
@@ -4,7 +4,9 @@ using static CoroutineHelper;
 
 public class AquariusShooter01 : EnemyShooter<EnemyBullet>
 {
-    const int WaveCount = 2;
+    const int WaveCount = 3;
+    const int BranchCount = 4;
+    const float BranchSpacing = 360f / BranchCount;
     const int BulletCount = 3;
     const float BulletSpacing = 3f;
 
@@ -16,13 +18,17 @@ public class AquariusShooter01 : EnemyShooter<EnemyBullet>
 
         for (int i = 0; i < WaveCount; i++)
         {
-            for (int ii = 0; ii < BulletCount; ii++)
-            {
-                float r = PlayerPosition.GetRotationDifference(transform.position);
-                float z = ((ii - (BulletCount - 1) / 2) * BulletSpacing) + r;
-                Vector3 pos = Vector3.zero;
+            float r = PlayerPosition.GetRotationDifference(transform.position);
 
-                SpawnProjectile(1, z, pos).Fire();
+            for (int ii = 0; ii < BranchCount; ii++)
+            {
+                for (int iii = 0; iii < BulletCount; iii++)
+                {
+                    float z = ((iii - (BulletCount - 1) / 2) * BulletSpacing) + (ii * BranchSpacing) + r;
+                    Vector3 pos = Vector3.zero;
+
+                    SpawnProjectile(1, z, pos).Fire();
+                }
             }
 
             yield return WaitForSeconds(ShootingCooldown);
