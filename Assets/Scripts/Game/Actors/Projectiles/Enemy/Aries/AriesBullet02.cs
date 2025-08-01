8b512f6ccba41c9c7574390afdb51a1437201c9a
Yoshi1493 <nic.choy107@gmail.com>
2025-08-01 13:51:53 -0400

    Update AriesBullet02.cs
    updated lifetime

--- Assets/Scripts/Game/Actors/Projectiles/Enemy/Aries/AriesBullet02.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Enemy/Aries/AriesBullet02.cs
@@ -2,8 +2,6 @@ using System.Collections;
 
 public class AriesBullet02 : EnemyBullet
 {
-    protected override float MaxLifetime => 6f;
-
     protected override IEnumerator Move()
     {
         yield return this.LerpSpeed(6f, 0f, 0.5f);
