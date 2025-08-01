a4ab68920a740babe652bab53415749d1c6d45ed
Yoshi1493 <nic.choy107@gmail.com>
2025-08-01 19:03:04 -0400

    Update Aries CE data
    updated EB, EBS, SO, prefabs

---
+++ Assets/Scripts/Game/Actors/Projectiles/Enemy/Aries/AriesBullet08.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+
+public class AriesBullet08 : EnemyBullet
+{
+    protected override IEnumerator Move()
+    {
+        yield return null;
+    }
+}
--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Aries/AriesShooter01.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Aries/AriesShooter01.cs
@@ -10,7 +10,7 @@ public class AriesShooter01 : EnemyShooter<EnemyBullet>
     const float BranchSpacing = 8f;
     const int BulletCount = 2;
     const float BulletRotationDuration = 2f;
-    const float BulletBaseSpeed = 3f;
+    const float BulletBaseSpeed = 2f;
     const float BulletSpeedModifier = 1f;
 
     List<EnemyBullet> bullets = new(BranchCount * BulletCount);
@@ -46,56 +46,4 @@ public class AriesShooter01 : EnemyShooter<EnemyBullet>
             bullets.Clear();
         }
     }
-
-    //const int BranchCount = 5;
-    //const float BranchSpacing = 5f;
-    //const int BulletCount = 3;
-    //const float BulletBaseSpeed = 4f;
-    //const float BulletSpeedModifier = 0.6f;
-
-    //List<EnemyBullet> bullets = new(BranchCount * BulletCount);
-
-    //protected override IEnumerator Shoot()
-    //{
-    //    bullets.Clear();
-
-    //    yield return WaitForSeconds(1f);
-
-    //    for (int i = 0; i < BranchCount; i++)
-    //    {
-    //        for (int ii = 0; ii < BulletCount; ii++)
-    //        {
-    //            float z = PlayerPosition.GetRotationDifference(transform.position);
-    //            Vector3 pos = Vector3.zero;
-
-    //            bulletData.colour = bulletData.gradient.Evaluate(ii / (BulletCount - 1f));
-
-    //            var bullet = SpawnProjectile(1, z, pos);
-    //            bullet.StartCoroutine(bullet.LerpSpeed(BulletBaseSpeed * 2f, 0f, 0.5f));
-    //            bullets.Add(bullet);
-    //        }
-    //    }
-
-    //    yield return WaitForSeconds(0.5f);
-
-    //    for (int i = 0; i < BranchCount; i++)
-    //    {
-    //        for (int ii = 0; ii < BulletCount; ii++)
-    //        {
-    //            int b = (i * BulletCount) + ii;
-
-    //            if (bullets[b].isActiveAndEnabled)
-    //            {
-    //                float z = (i - ((BranchCount - 1) / 2)) * BranchSpacing;
-    //                float s = BulletBaseSpeed + (ii * BulletSpeedModifier);
-
-    //                bullets[b].StartCoroutine(bullets[b].RotateBy(z, 0.5f));
-    //                bullets[b].StartCoroutine(bullets[b].LerpSpeed(0f, s, 1f));
-    //            }
-    //        }
-    //    }
-
-    //    yield return WaitForSeconds(ShootingCooldown);
-
-    //}
 }
