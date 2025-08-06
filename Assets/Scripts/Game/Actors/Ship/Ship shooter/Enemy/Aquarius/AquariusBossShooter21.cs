953cff8c662ae06e001bb6ae579dba911cb6c56d
Yoshi1493 <nic.choy107@gmail.com>
2025-08-06 15:26:23 -0400

    Update Aquarius Boss data
    updated EBS, SO

--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Aquarius/AquariusBossShooter21.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Aquarius/AquariusBossShooter21.cs
@@ -5,6 +5,7 @@ using static CoroutineHelper;
 public class AquariusBossShooter21 : BossShooter<BossBullet>
 {
     const int WaveCount = 5;
+    const float WaveSpacing = 1f;
     const int BranchCount = 5;
     const float BranchSpacing = 10f;
 
@@ -12,18 +13,20 @@ public class AquariusBossShooter21 : BossShooter<BossBullet>
 
     protected override IEnumerator Shoot()
     {
-        while (enabled)
+        for (int i = 1; enabled; i *= -1)
         {
             yield return WaitForSeconds(1f);
 
             float r = PlayerPosition.GetRotationDifference(transform.position);
 
-            for (int i = 0; i < WaveCount; i++)
+            for (int ii = 0; ii < WaveCount; ii++)
             {
-                for (int ii = 0; ii < BranchCount; ii++)
+                for (int iii = 0; iii < BranchCount; iii++)
                 {
-                    float z = ((ii - ((BranchCount - 1) / 2f)) * BranchSpacing) + r;
+                    float z = (i * (ii - ((WaveCount - 1) / 2f)) * WaveSpacing) + ((iii - ((BranchCount - 1) / 2f)) * BranchSpacing) + r;
                     Vector3 pos = Vector3.zero;
+
+                    bulletData.colour = bulletData.gradient.Evaluate(ii / (BranchCount - 1f));
 
                     SpawnProjectile(1, z, pos).Fire();
                 }
