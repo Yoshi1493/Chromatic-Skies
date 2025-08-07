97517ec287318229b3b587c39247eb4b59d5ee9e
Yoshi1493 <nic.choy107@gmail.com>
2025-08-07 13:47:06 -0400

    Revert Capricon EBS to template code

--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Capricorn/CapricornShooter04.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Capricorn/CapricornShooter04.cs
@@ -4,18 +4,15 @@ using static CoroutineHelper;
 
 public class CapricornShooter04 : CommonEnemyShooter<EnemyBullet>
 {
-    const int BulletCount = 5;
-    const float BulletSpacing = 20f;
+    const int BulletCount = 0;
 
     protected override IEnumerator Shoot()
     {
         yield return WaitForSeconds(1f);
 
-        float r = PlayerPosition.GetRotationDifference(transform.position);
-
         for (int i = 0; i < BulletCount; i++)
         {
-            float z = ((i - ((BulletCount - 1) / 2f)) * BulletSpacing) + r;
+            float z = 0f;
             Vector3 pos = Vector3.zero;
 
             SpawnProjectile(0, z, pos).Fire();
