dbb9af3eb4b40ddc00c8aedaf7c176b06c2ca9a0
Yoshi1493 <nic.choy107@gmail.com>
2025-08-11 17:05:08 -0400

    Update CapricornBullet01.cs

--- Assets/Scripts/Game/Actors/Projectiles/Enemy/Capricorn/CapricornBullet01.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Enemy/Capricorn/CapricornBullet01.cs
@@ -2,6 +2,8 @@ using System.Collections;
 
 public class CapricornBullet01 : EnemyBullet
 {
+    protected override float MaxLifetime => 8f;
+
     protected override IEnumerator Move()
     {
         yield return this.LerpSpeed(6f, 0f, 0.5f);
