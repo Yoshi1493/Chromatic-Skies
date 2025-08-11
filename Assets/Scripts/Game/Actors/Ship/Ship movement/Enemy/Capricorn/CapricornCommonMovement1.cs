8ede9f2c9facf2ed15d65cf1558aa87f7caa220c
Yoshi1493 <nic.choy107@gmail.com>
2025-08-11 17:03:50 -0400

    Update CapricornCommonMovement1.cs

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Capricorn/CapricornCommonMovement1.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Capricorn/CapricornCommonMovement1.cs
@@ -6,12 +6,8 @@ public class CapricornCommonMovement1 : CommonEnemyMovement
 {
     protected override IEnumerator Move()
     {
-        Vector3 pos = parentShip.transform.position;
-        float d = -Mathf.Sign(pos.x);
-
         yield return parentShip.MoveRelative(Vector3.down, 3f, 1.2f);
         yield return WaitForSeconds(12f);
-        yield return parentShip.MoveRelative(Vector3.down.RotateVectorBy(d * 45f), 2f, 6f);
 
         yield return LeaveScene();
     }
