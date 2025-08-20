426177095db7f2be0750e570867a3dba5a372169
Yoshi1493 <nic.choy107@gmail.com>
2025-08-19 23:09:10 -0400

    Add Projectile graze SFX
    impl.'d sound in PSPlayerGraze; updated AudioManager in Game scene accordingly

--- Assets/Scripts/Game/VFX/Particle effects/PSPlayerGraze.cs
+++ Assets/Scripts/Game/VFX/Particle effects/PSPlayerGraze.cs
@@ -1,7 +1,20 @@
 public class PSPlayerGraze : ParticleEffect
 {
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        PlaySound();
+    }
+
+    void PlaySound()
+    {
+        AudioManager.Instance.PlaySound("projectile_graze", true);
+    }
+
     protected override void ReturnToPool()
     {
         VFXObjectPool.Instance.ReturnToPool(this, (int)VFXType.PlayerGraze);
     }
+
+
 }
