75fbb1b7fadacaf6410354c79073debdd6bd3f21
Yoshi1493 <nic.choy107@gmail.com>
2025-08-20 22:11:56 -0400

    Update Capricorn CE data
    updated EB, EBS

--- Assets/Scripts/Game/Actors/Projectiles/Enemy/Capricorn/CapricornBullet09.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Enemy/Capricorn/CapricornBullet09.cs
@@ -4,6 +4,6 @@ public class CapricornBullet09 : EnemyBullet
 {
     protected override IEnumerator Move()
     {
-        yield return this.LerpSpeed(0.5f, 4f, 2f);
+        yield return this.LerpSpeed(5f, 0f, 0.5f);
     }
 }
--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Capricorn/CapricornShooter09.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Capricorn/CapricornShooter09.cs
@@ -6,6 +6,10 @@ public class CapricornShooter09 : CommonEnemyShooter<EnemyBullet>
 {
     const int WaveCount = 20;
     const float WaveSpacing = 8f;
+    const int BulletCount = 2;
+    const float BulletBaseSpeed = 2f;
+    const float BulletSpeedModifier = 0.1f;
+
     protected override float ShootingCooldown => 0.05f;
 
     protected override IEnumerator Shoot()
@@ -16,13 +20,20 @@ public class CapricornShooter09 : CommonEnemyShooter<EnemyBullet>
 
         for (int i = 0; i < WaveCount; i++)
         {
-            float z = ((i - ((WaveCount - 1) / 2f)) * WaveSpacing) + r;
-            Vector3 pos = Vector3.zero;
+            for (int ii = 0; ii < BulletCount; ii++)
+            {
+                float z = ((i - ((WaveCount - 1) / 2f)) * WaveSpacing) + r;
+                float s = (ii % 2 * WaveCount * BulletSpeedModifier) + BulletBaseSpeed + (-(ii % 2 * 2 - 1) * (i * BulletSpeedModifier));
+                Vector3 pos = Vector3.zero;
 
-            SpawnProjectile(0, z, pos).Fire();
+                bulletData.colour = bulletData.gradient.Evaluate((ii % 2) + (-(ii % 2 * 2 - 1) * i / (WaveCount - 1f)));
+
+                var bullet = SpawnProjectile(9, z, pos);
+                bullet.StartCoroutine(bullet.LerpSpeed(0f, s, 1f, delay: 0.5f));
+                bullet.Fire();
+            }
 
             yield return WaitForSeconds(ShootingCooldown);
         }
-
     }
 }
