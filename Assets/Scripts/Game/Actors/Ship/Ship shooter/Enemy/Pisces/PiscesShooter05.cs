c3286eab824826fe1cf95fc11efe6eb99734f29b
Yoshi1493 <nic.choy107@gmail.com>
2025-09-04 12:59:09 -0400

    Update PiscesShooter05.cs
    fix EBS

--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Pisces/PiscesShooter05.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Pisces/PiscesShooter05.cs
@@ -13,21 +13,19 @@ public class PiscesShooter05 : CommonEnemyShooter<EnemyBullet>
 
     protected override IEnumerator Shoot()
     {
-        yield return WaitForSeconds(1f);
-
         for (int i = 0; i < RepeatCount; i++)
         {
+            yield return WaitForSeconds(0.6f);
+
             for (int ii = 0; ii < BulletCount; ii++)
             {
                 Vector3 pos = BulletSpawnRadius * transform.up.RotateVectorBy(RandomAngleDeg);
-                float z = PlayerPosition.GetRotationDifference(pos);
+                float z = PlayerPosition.GetRotationDifference(transform.position + pos);
 
-                SpawnProjectile(5, z, pos).Fire();
+                SpawnProjectile(0, z, pos).Fire();
 
                 yield return WaitForSeconds(ShootingCooldown);
             }
-
-            yield return WaitForSeconds(1f);
         }
     }
 }
