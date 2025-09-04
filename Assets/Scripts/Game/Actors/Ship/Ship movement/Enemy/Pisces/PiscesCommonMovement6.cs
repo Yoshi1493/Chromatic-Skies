d6164a869194bb003b8dc373d1c4bd9da6bda3f1
Yoshi1493 <nic.choy107@gmail.com>
2025-09-04 15:40:39 -0400

    Update PiscesCommonMovement6.cs

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Pisces/PiscesCommonMovement6.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Pisces/PiscesCommonMovement6.cs
@@ -1,12 +1,9 @@
 using System.Collections;
-using UnityEngine;
-using static CoroutineHelper;
 
 public class PiscesCommonMovement6 : CommonEnemyMovement
 {
     protected override IEnumerator Move()
     {
         yield return parentShip.MoveTo(ActorMovementHelper.bossSpawnPosition, 1.5f);
-
     }
 }
