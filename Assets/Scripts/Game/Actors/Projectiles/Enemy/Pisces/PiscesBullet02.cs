62afcbd3de7e9e032d8ad071b2f7ba6886687f9e
Yoshi1493 <nic.choy107@gmail.com>
2025-09-06 21:49:49 -0400

    Update Pisces CE data
    updated EB, EBS

--- Assets/Scripts/Game/Actors/Projectiles/Enemy/Pisces/PiscesBullet02.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Enemy/Pisces/PiscesBullet02.cs
@@ -7,6 +7,6 @@ public class PiscesBullet02 : EnemyBullet
     protected override IEnumerator Move()
     {
         yield return this.LerpSpeed(5f, 0.5f, 0.5f);
-        yield return this.LerpSpeed(0.5f, 2.5f, 1f);
+        yield return this.LerpSpeed(0.5f, 3f, 1f);
     }
 }
--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Pisces/PiscesShooter02.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Pisces/PiscesShooter02.cs
@@ -5,33 +5,25 @@ using static CoroutineHelper;
 public class PiscesShooter02 : CommonEnemyShooter<EnemyBullet>
 {
     const int WaveCount = 8;
-    const int BranchCount = 1;
-    const float BranchSpacing = 10f;
     const int BulletCount = 3;
+    const float BulletSpawnRadius = 0.5f;
     const float BulletRotationSpeed = 5f;
     const float BulletRotationDuration = 1f;
 
-    protected override float ShootingCooldown => 0.2f;
-
     protected override IEnumerator Shoot()
     {
-        yield return WaitForSeconds(1.5f);
+        yield return WaitForSeconds(1.2f);
 
         for (int i = 0; i < WaveCount; i++)
         {
-            float r = PlayerPosition.GetRotationDifference(transform.position);
+            float z = PlayerPosition.GetRotationDifference(transform.position);
+            Vector3 pos = BulletSpawnRadius * Random.insideUnitCircle;
 
-            for (int ii = 0; ii < BranchCount; ii++)
+            for (int ii = 0; ii < BulletCount; ii++)
             {
-                for (int iii = 0; iii < BulletCount; iii++)
-                {
-                    float z = ((ii - ((BranchCount - 1) / 2f)) * BranchSpacing) + r;
-                    Vector3 pos = Vector3.zero;
-
-                    var bullet = SpawnProjectile(0, z, pos);
-                    bullet.StartCoroutine(bullet.RotateBy(Random.Range(-BulletRotationSpeed, BulletRotationSpeed), BulletRotationDuration));
-                    bullet.Fire();
-                }
+                var bullet = SpawnProjectile(2, z, pos);
+                bullet.StartCoroutine(bullet.RotateBy(Random.Range(-BulletRotationSpeed, BulletRotationSpeed), BulletRotationDuration));
+                bullet.Fire();
             }
 
             yield return WaitForSeconds(ShootingCooldown);
