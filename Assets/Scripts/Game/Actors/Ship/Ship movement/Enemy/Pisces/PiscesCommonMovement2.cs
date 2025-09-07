642e49e80086c6b332d603f23728a04c1a42180d
Yoshi1493 <nic.choy107@gmail.com>
2025-09-06 22:15:15 -0400

    Update Pisces CE data
    updated EBS, MS

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Pisces/PiscesCommonMovement2.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Pisces/PiscesCommonMovement2.cs
@@ -10,15 +10,16 @@ public class PiscesCommonMovement2 : CommonEnemyMovement
         Player player = FindObjectOfType<Player>();
         float d = Mathf.Sign(player.transform.position.x - transform.position.x);
 
+        AnimationCurve movementInterpolation = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+
         Vector2[] movementPoints = new Vector2[]
         {
             transform.position,
             transform.position + (d * 2f * Vector3.right),
-            new(transform.position.x + (d * 4f), 4f),
-            new(transform.position.x + (d * 4f), 2f)
+            new(transform.position.x + (d * 4f), 3f),
+            new(transform.position.x + (d * 4f), 1f)
         };
 
-        AnimationCurve movementInterpolation = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
         float t = 0f;
 
         while (t < (movementPoints.Length - 1) / 3)
@@ -26,10 +27,25 @@ public class PiscesCommonMovement2 : CommonEnemyMovement
             parentShip.transform.position = EvaluateCubicSpline(movementPoints, movementInterpolation.Evaluate(t));
 
             yield return null;
-            t += Time.deltaTime;
+            t += Time.deltaTime / 2f;
         }
 
-        yield return WaitForSeconds(2f);
+        yield return WaitForSeconds(1.5f);
+
+        movementPoints[0] = transform.position;
+        movementPoints[1] = transform.position + (2f * Vector3.up);
+        movementPoints[2] = new(transform.position.x + (d * 2f), 6f);
+        movementPoints[3] = new(transform.position.x + (d * 4f), 6f);
+
+        t = 0f;
+
+        while (t < (movementPoints.Length - 1) / 3)
+        {
+            parentShip.transform.position = EvaluateCubicSpline(movementPoints, movementInterpolation.Evaluate(t));
+
+            yield return null;
+            t += Time.deltaTime / 2f;
+        }
 
         yield return LeaveScene();
     }
--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Pisces/PiscesShooter02.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Pisces/PiscesShooter02.cs
@@ -4,7 +4,7 @@ using static CoroutineHelper;
 
 public class PiscesShooter02 : CommonEnemyShooter<EnemyBullet>
 {
-    const int WaveCount = 8;
+    const int WaveCount = 16;
     const int BulletCount = 3;
     const float BulletSpawnRadius = 0.5f;
     const float BulletRotationSpeed = 5f;
@@ -12,7 +12,7 @@ public class PiscesShooter02 : CommonEnemyShooter<EnemyBullet>
 
     protected override IEnumerator Shoot()
     {
-        yield return WaitForSeconds(1.2f);
+        yield return WaitForSeconds(1.5f);
 
         for (int i = 0; i < WaveCount; i++)
         {
@@ -21,8 +21,8 @@ public class PiscesShooter02 : CommonEnemyShooter<EnemyBullet>
 
             for (int ii = 0; ii < BulletCount; ii++)
             {
-                var bullet = SpawnProjectile(2, z, pos);
-                bullet.StartCoroutine(bullet.RotateBy(Random.Range(-BulletRotationSpeed, BulletRotationSpeed), BulletRotationDuration));
+                var bullet = SpawnProjectile(0, z, pos);
+                bullet.StartCoroutine(bullet.RotateBy(Random.Range(-BulletRotationSpeed, BulletRotationSpeed), BulletRotationDuration, delay: 1f));
                 bullet.Fire();
             }
 
