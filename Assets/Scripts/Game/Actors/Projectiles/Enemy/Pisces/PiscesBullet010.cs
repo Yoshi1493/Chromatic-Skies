1f01b9c49b56175155cc0931b025e9518fa934ba
Yoshi1493 <nic.choy107@gmail.com>
2025-09-09 21:34:27 -0400

    Update Pisces CE data
    updated EB, EBS, SO

--- Assets/Scripts/Game/Actors/Projectiles/Enemy/Pisces/PiscesBullet010.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Enemy/Pisces/PiscesBullet010.cs
@@ -1,11 +1,13 @@
 using System.Collections;
+using static CoroutineHelper;
 
 public class PiscesBullet010 : EnemyBullet
 {
-    protected override float MaxLifetime => 6f;
+    protected override float MaxLifetime => 8f;
 
     protected override IEnumerator Move()
     {
-        yield return this.LerpSpeed(5f, 0f, 0.5f);
+        yield return WaitForSeconds(1f);
+        yield return this.LerpSpeed(0f, 2.5f, 1f);
     }
 }
--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Pisces/PiscesShooter010.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Pisces/PiscesShooter010.cs
@@ -4,33 +4,43 @@ using static CoroutineHelper;
 
 public class PiscesShooter010 : CommonEnemyShooter<EnemyBullet>
 {
-    const int WaveCount = 21;
-    const float WaveSpacing = 8f;
+    const int WaveCount = 8;
+    const float WaveSpacing = 2f;
+    const int BranchCount = 6;
+    const float BranchSpacing = 360f / BranchCount;
     const int BulletCount = 2;
+    const float BulletSpacing = 45f;
+    const float BulletSpawnRadius = 0.2f;
+    const float SpawnRadiusModifier = 0.4f;
     const float BulletBaseSpeed = 2f;
-    const float BulletSpeedModifier = 0.1f;
-
-    protected override float ShootingCooldown => 0.05f;
+    const float BulletSpeedModifier = 0.3f;
+    const float BulletRotationSpeedModifier = 5f;
+    const float BulletRotationDuration = 1f;
 
     protected override IEnumerator Shoot()
     {
         yield return WaitForSeconds(1.5f);
 
-        float r = PlayerPosition.GetRotationDifference(transform.position);
-
         for (int i = 0; i < WaveCount; i++)
         {
-            for (int ii = 0; ii < BulletCount; ii++)
+            for (int ii = 0; ii < BranchCount; ii++)
             {
-                float z = ((i - ((WaveCount - 1) / 2f)) * WaveSpacing) + r;
-                float s = (ii % 2 * WaveCount * BulletSpeedModifier) + BulletBaseSpeed + (-(ii % 2 * 2 - 1) * (i * BulletSpeedModifier));
-                Vector3 pos = Vector3.zero;
+                float t = ii * BranchSpacing;
 
-                bulletData.colour = bulletData.gradient.Evaluate(i / (WaveCount - 1f));
+                for (int iii = 0; iii < BulletCount; iii++)
+                {
+                    float z = (i * WaveSpacing) + t + ((iii % 2 * 2 - 1) * BulletSpacing);
+                    float s = BulletBaseSpeed + (i * BulletSpeedModifier);
+                    float r = (iii % 2 * 2 - 1) * (((WaveCount - 1) / 2f * -BulletRotationSpeedModifier) + (i * BulletRotationSpeedModifier));
+                    Vector3 pos = (BulletSpawnRadius + (i * SpawnRadiusModifier)) * transform.up.RotateVectorBy(t);
 
-                var bullet = SpawnProjectile(10, z, pos);
-                bullet.StartCoroutine(bullet.LerpSpeed(0f, s, 1f, delay: 0.5f));
-                bullet.Fire();
+                    bulletData.colour = bulletData.gradient.Evaluate(i / (WaveCount - 1f));
+
+                    var bullet = SpawnProjectile(10, z, pos);
+                    bullet.StartCoroutine(bullet.LerpSpeed(s, 0f, 1f));
+                    bullet.StartCoroutine(bullet.RotateBy(r, BulletRotationDuration, delay: 1f));
+                    bullet.Fire();
+                }
             }
 
             yield return WaitForSeconds(ShootingCooldown);
