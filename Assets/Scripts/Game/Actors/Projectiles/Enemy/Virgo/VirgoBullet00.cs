3469aa2ac52aca0f14631649e2586c7d6dd0cc4a
Yoshi1493 <nic.choy107@gmail.com>
2025-09-12 14:17:22 -0400

    Update Virgo CE data
    updated EB, EBS, MS

--- Assets/Scripts/Game/Actors/Projectiles/Enemy/Virgo/VirgoBullet00.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Enemy/Virgo/VirgoBullet00.cs
@@ -4,6 +4,7 @@ public class VirgoBullet00 : EnemyBullet
 {
     protected override IEnumerator Move()
     {
-        yield return null;
+        yield return this.LerpSpeed(4f, 1f, 1f);
+        yield return this.LerpSpeed(1f, 3f, 2f);
     }
 }
--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Virgo/VirgoCommonMovement0.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Virgo/VirgoCommonMovement0.cs
@@ -6,6 +6,13 @@ public class VirgoCommonMovement0 : CommonEnemyMovement
 {
     protected override IEnumerator Move()
     {
+        Vector3 rotationPoint = 6f * Vector3.up;
+        float d = -SignX;
+
+        yield return parentShip.TranslateAround(rotationPoint, d * 90f, 2f);
+        yield return WaitForSeconds(1f);
+        yield return parentShip.TranslateAround(rotationPoint, d * 90f, 2f);
+
         yield return LeaveScene();
     }
 }
