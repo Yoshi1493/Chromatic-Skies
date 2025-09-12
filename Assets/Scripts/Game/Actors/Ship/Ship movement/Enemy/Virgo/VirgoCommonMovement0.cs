c8cd3dd7db3449cee7cf30b526d74fd5a93afb7a
Yoshi1493 <nic.choy107@gmail.com>
2025-09-12 14:58:16 -0400

    Update VirgoCommonMovement0.cs

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Virgo/VirgoCommonMovement0.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Virgo/VirgoCommonMovement0.cs
@@ -6,12 +6,12 @@ public class VirgoCommonMovement0 : CommonEnemyMovement
 {
     protected override IEnumerator Move()
     {
-        Vector3 rotationPoint = 6f * Vector3.up;
+        Vector3 rotationPoint = new(parentShip.transform.position.x + (-SignX * 5f), parentShip.transform.position.y);
         float d = -SignX;
 
-        yield return parentShip.TranslateAround(rotationPoint, d * 90f, 2f);
+        yield return parentShip.TranslateAround(rotationPoint, d * 90f, 2.5f);
         yield return WaitForSeconds(1f);
-        yield return parentShip.TranslateAround(rotationPoint, d * 90f, 2f);
+        yield return parentShip.TranslateAround(rotationPoint, d * 90f, 2.5f);
 
         yield return LeaveScene();
     }
