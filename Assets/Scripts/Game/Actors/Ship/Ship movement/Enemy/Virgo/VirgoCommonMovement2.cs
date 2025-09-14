5027cfbc13f7a7480dd54a76a3d8dbad4ba1bd1a
Yoshi1493 <nic.choy107@gmail.com>
2025-09-14 17:16:52 -0400

    Update Virgo CE data
    updated EB, EBS, SO, MS

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Virgo/VirgoCommonMovement2.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Virgo/VirgoCommonMovement2.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using UnityEngine;
-using static CoroutineHelper;
 
 public class VirgoCommonMovement2 : CommonEnemyMovement
 {
     protected override IEnumerator Move()
     {
+        float r = Random.Range(-30f, 30f);
+
+        yield return parentShip.MoveRelative(Vector3.down.RotateVectorBy(r), 1.5f, 3f);
+        yield return null;
+        yield return parentShip.MoveRelativeLinear(Vector3.down.RotateVectorBy(-r), 2f, 5f);
+
         yield return LeaveScene();
     }
 }
