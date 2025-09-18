29d15bcb16276809eb5910d680629cbdd8a51448
Yoshi1493 <nic.choy107@gmail.com>
2025-09-17 20:46:03 -0400

    Update Virgo CE data
    updated MS

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Virgo/VirgoCommonMovement0.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Virgo/VirgoCommonMovement0.cs
@@ -8,10 +8,11 @@ public class VirgoCommonMovement0 : CommonEnemyMovement
     {
         Vector3 rotationPoint = new(parentShip.transform.position.x + (-SignX * 5f), parentShip.transform.position.y);
         float d = -SignX;
+        float r = 90f + Random.Range(-15f, 15f);
 
-        yield return parentShip.TranslateAround(rotationPoint, d * 90f, 2.5f);
+        yield return parentShip.TranslateAround(rotationPoint, d * r, 2.5f);
         yield return WaitForSeconds(1f);
-        yield return parentShip.TranslateAround(rotationPoint, d * 90f, 2.5f);
+        yield return parentShip.TranslateAround(rotationPoint, d * (180f - r), 2.5f);
 
         yield return LeaveScene();
     }
--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Virgo/VirgoCommonMovement5.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Virgo/VirgoCommonMovement5.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using UnityEngine;
 using static CoroutineHelper;
 
 public class VirgoCommonMovement5 : CommonEnemyMovement
