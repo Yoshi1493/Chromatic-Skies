ce4e86000b850fa409083fbafcf691f52638f617
Yoshi1493 <nic.choy107@gmail.com>
2025-09-18 18:17:50 -0400

    Update Virgo CE data
    updated EB, EBS, SO, MS

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Virgo/VirgoCommonMovement6.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Virgo/VirgoCommonMovement6.cs
@@ -1,11 +1,13 @@
 using System.Collections;
-using UnityEngine;
 using static CoroutineHelper;
 
 public class VirgoCommonMovement6 : CommonEnemyMovement
 {
     protected override IEnumerator Move()
     {
+        yield return parentShip.SpiralIntoPoint(new(-4f, 2f), -180f, 1.5f);
+        yield return WaitForSeconds(6f);
+
         yield return LeaveScene();
     }
 }
--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Virgo/VirgoShooter07.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Virgo/VirgoShooter07.cs
@@ -1,24 +1,36 @@
 using System.Collections;
 using UnityEngine;
 using static CoroutineHelper;
+using static MathHelper;
 
 public class VirgoShooter07 : CommonEnemyShooter<EnemyBullet>
 {
-    const int BulletCount = 0;
+    const int WaveCount = 36;
+    const int BulletMinCount = 3;
+    const int BulletMaxCount = 7;
+    const float BulletSpawnRadius = 1f;
+    const float SpawnRadiusModifier = -0.05f;
+
+    protected override float ShootingCooldown => 0.05f;
 
     protected override IEnumerator Shoot()
     {
-        while (enabled)
+        yield return WaitForSeconds(1.5f);
+
+        for (int ii = 0; ii < WaveCount; ii++)
         {
-            for (int i = 0; i < BulletCount; i++)
+            int bulletCount = Random.Range(BulletMinCount, BulletMaxCount);
+
+            for (int iv = 0; iv < bulletCount; iv++)
             {
-                float z = 0;
-                Vector3 pos = Vector3.zero;
+                float z = RandomAngleDeg;
+                Vector3 pos = (BulletSpawnRadius + (ii * SpawnRadiusModifier)) * transform.up.RotateVectorBy(z);
 
                 SpawnProjectile(0, z, pos).Fire();
             }
 
             yield return WaitForSeconds(ShootingCooldown);
         }
+
     }
 }
