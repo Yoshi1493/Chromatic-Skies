a11e89edabd8a71051a6c225688ec932cc4472d8
Yoshi1493 <nic.choy107@gmail.com>
2025-10-30 21:44:30 -0400

    Update CancerCommonMovement1.cs

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Cancer/CancerCommonMovement1.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Cancer/CancerCommonMovement1.cs
@@ -7,7 +7,7 @@ public class CancerCommonMovement1 : CommonEnemyMovement
     protected override IEnumerator Move()
     {
         yield return parentShip.SpiralIntoPoint(parentShip.transform.position + 4f * Vector3.down.RotateVectorBy(-SignX * 10f), SignX * 45f, 1.5f);
-        yield return WaitForSeconds(5f);
+        yield return WaitForSeconds(8f);
 
         yield return LeaveScene();
     }
