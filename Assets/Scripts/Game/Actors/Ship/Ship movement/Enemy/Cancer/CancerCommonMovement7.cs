c7c6cfaee8826d785aac57e8512725251f15b13a
Yoshi1493 <nic.choy107@gmail.com>
2025-11-07 19:12:45 -0500

    Update Cancer CE data
    updated EB, EBS, SO, MS

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Cancer/CancerCommonMovement7.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Cancer/CancerCommonMovement7.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using UnityEngine;
-using static CoroutineHelper;
 
 public class CancerCommonMovement7 : CommonEnemyMovement
 {
     protected override IEnumerator Move()
     {
+        Vector3 originalPosition = parentShip.transform.position;
+        float d = SignX;
+
+        yield return parentShip.SpiralIntoPoint(originalPosition + new Vector3(d * -2f, -13f), d * 30f, 6f);
+
         yield return LeaveScene();
     }
 }
--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Cancer/CancerShooter08.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Cancer/CancerShooter08.cs
@@ -4,15 +4,19 @@ using static CoroutineHelper;
 
 public class CancerShooter08 : CommonEnemyShooter<EnemyBullet>
 {
-    const int BulletCount = 0;
+    const int WaveCount = 44;
+    const float WaveSpacing = 15f;
+    const int BulletCount = 2;
 
     protected override IEnumerator Shoot()
     {
-        while (enabled)
+        yield return WaitForSeconds(1f);
+
+        for (int i = 0; i < WaveCount; i++)
         {
-            for (int i = 0; i < BulletCount; i++)
+            for (int ii = 0; ii < BulletCount; ii++)
             {
-                float z = 0;
+                float z = ((ii % 2 * 2 - 1) * i * WaveSpacing) + 180f;
                 Vector3 pos = Vector3.zero;
 
                 SpawnProjectile(0, z, pos).Fire();
