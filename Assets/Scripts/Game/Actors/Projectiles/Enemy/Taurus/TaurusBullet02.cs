989fad6c6f66c36a0eab92b8d95a284b373e32a5
Yoshi1493 <nic.choy107@gmail.com>
2025-11-14 12:45:48 -0500

    Fix Taurus CE data
    fixed EB, SO, prefab fields
    updated miniboss prefab with (temp) sprite
    updated Game scene

--- Assets/Scripts/Game/Actors/Projectiles/Enemy/Taurus/TaurusBullet02.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Enemy/Taurus/TaurusBullet02.cs
@@ -4,6 +4,13 @@ public class TaurusBullet02 : EnemyBullet
 {
     protected override IEnumerator Move()
     {
-        yield return null;
+        while (enabled)
+        {
+            StartCoroutine(this.RotateBy(45f, 0f));
+            yield return this.LerpSpeed(4f, 0f, 1f);
+
+            StartCoroutine(this.RotateBy(-45f, 0f));
+            yield return this.LerpSpeed(4f, 0f, 1f);
+        }
     }
 }
--- Assets/Scripts/Game/Actors/Projectiles/Enemy/Taurus/TaurusBullet03.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Enemy/Taurus/TaurusBullet03.cs
@@ -2,15 +2,10 @@ using System.Collections;
 
 public class TaurusBullet03 : EnemyBullet
 {
+    protected override float MaxLifetime => 6f;
+
     protected override IEnumerator Move()
     {
-        while (enabled)
-        {
-            StartCoroutine(this.RotateBy(45f, 0f));
-            yield return this.LerpSpeed(4f, 0f, 1f);
-
-            StartCoroutine(this.RotateBy(-45f, 0f));
-            yield return this.LerpSpeed(4f, 0f, 1f);
-        }
+        yield return this.LerpSpeed(1f, 3.5f, 2f);
     }
 }
