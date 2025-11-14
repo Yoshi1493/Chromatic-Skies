c419b73aad556af162751b8a9b3ed4f18dd60125
Yoshi1493 <nic.choy107@gmail.com>
2025-11-14 14:46:26 -0500

    Update TaurusCommonMovement1.cs

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Taurus/TaurusCommonMovement1.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Taurus/TaurusCommonMovement1.cs
@@ -5,7 +5,10 @@ public class TaurusCommonMovement1 : CommonEnemyMovement
 {
     protected override IEnumerator Move()
     {
-        yield return parentShip.MoveRelative(Vector3.down.RotateVectorBy(-SignX * 15f), 4f, 3f);
+        float d = SignX;
+
+        parentShip.transform.position += d * Random.Range(0f, 2f) * Vector3.right;
+        yield return parentShip.MoveRelative(Vector3.down.RotateVectorBy(-d * 15f), 4f, 3f);
 
         yield return LeaveScene();
     }
