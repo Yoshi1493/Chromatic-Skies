ce29c3c53ba2e8aace5e282597267f43dd6aad12
Yoshi1493 <nic.choy107@gmail.com>
2025-11-25 16:01:22 -0500

    Update TaurusShooter09.cs

--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Taurus/TaurusShooter09.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Taurus/TaurusShooter09.cs
@@ -7,7 +7,7 @@ public class TaurusShooter09 : CommonEnemyShooter<Laser>
     const int WaveCount = 10;
     const float WaveSpacing = 360f / WaveCount;
     const int LaserCount = 2;
-    const float LaserSpacing = 32f;
+    const float LaserSpacing = 30f;
     const float BulletSpawnRadius = 0.8f;
 
     protected override float ShootingCooldown => 0.05f;
@@ -16,20 +16,23 @@ public class TaurusShooter09 : CommonEnemyShooter<Laser>
     {
         yield return WaitForSeconds(1.5f);
 
-        float r = transform.position.GetRotationDifference(PlayerPosition);
+        Vector3 v0 = parentShip.transform.position;
+        Vector3 v1 = PlayerPosition;
+        float r = transform.position.GetRotationDifference(v1);
 
         for (int i = 0; i < WaveCount; i++)
         {
             for (int ii = 0; ii < LaserCount; ii++)
             {
-                int d = ii % 2 * 2 - 1;
-                float z = d * ((i * WaveSpacing) + LaserSpacing) + r;
-                float t = z + (d * -30f);
-                Vector3 pos = BulletSpawnRadius * transform.up.RotateVectorBy(t);
+                float d = ((ii % 2 * 2 - 1) * LaserSpacing) + r;
+                Vector3 pos = v0 + (BulletSpawnRadius * transform.up.RotateVectorBy(d));
+                float t = (i + 1) * WaveSpacing;
+                float z = t + pos.GetRotationDifference(v1);
+                pos = (pos - v0).RotateVectorBy(t) + v0;
 
                 bulletData.colour = bulletData.gradient.Evaluate(i / (WaveCount - 1f));
 
-                SpawnProjectile(0, z, pos).Fire();
+                SpawnProjectile(0, z, pos, false).Fire();
             }
 
             yield return WaitForSeconds(ShootingCooldown);
