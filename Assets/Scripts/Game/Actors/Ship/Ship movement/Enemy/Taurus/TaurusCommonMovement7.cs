148b61bf575b9cc1029f23ed136e1e2172759a55
Yoshi1493 <nic.choy107@gmail.com>
2025-11-25 00:11:59 -0500

    Update Taurus CE data
    updated EB, EBS, SO, MS, prefabs

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Taurus/TaurusCommonMovement7.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Taurus/TaurusCommonMovement7.cs
@@ -6,6 +6,12 @@ public class TaurusCommonMovement7 : CommonEnemyMovement
 {
     protected override IEnumerator Move()
     {
+        Vector3 p0 = parentShip.transform.position;
+        Vector3 p1 = new(-p0.x, 0f);
+
+        yield return parentShip.SpiralIntoPoint(p1, -45f, 1.5f);
+        yield return WaitForSeconds(3f);
+
         yield return LeaveScene();
     }
 }
--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Taurus/TaurusShooter09.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Taurus/TaurusShooter09.cs
@@ -2,18 +2,32 @@ using System.Collections;
 using UnityEngine;
 using static CoroutineHelper;
 
-public class TaurusShooter09 : CommonEnemyShooter<EnemyBullet>
+public class TaurusShooter09 : CommonEnemyShooter<Laser>
 {
-    const int BulletCount = 0;
+    const int WaveCount = 10;
+    const float WaveSpacing = 360f / WaveCount;
+    const int LaserCount = 2;
+    const float LaserSpacing = 32f;
+    const float BulletSpawnRadius = 0.8f;
+
+    protected override float ShootingCooldown => 0.05f;
 
     protected override IEnumerator Shoot()
     {
-        while (enabled)
+        yield return WaitForSeconds(1.5f);
+
+        float r = transform.position.GetRotationDifference(PlayerPosition);
+
+        for (int i = 0; i < WaveCount; i++)
         {
-            for (int i = 0; i < BulletCount; i++)
+            for (int ii = 0; ii < LaserCount; ii++)
             {
-                float z = 0;
-                Vector3 pos = Vector3.zero;
+                int d = ii % 2 * 2 - 1;
+                float z = d * ((i * WaveSpacing) + LaserSpacing) + r;
+                float t = z + (d * -30f);
+                Vector3 pos = BulletSpawnRadius * transform.up.RotateVectorBy(t);
+
+                bulletData.colour = bulletData.gradient.Evaluate(i / (WaveCount - 1f));
 
                 SpawnProjectile(0, z, pos).Fire();
             }
