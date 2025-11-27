b11cac5f4e250933721c5a4be4cb151fa8c30ae3
Yoshi1493 <nic.choy107@gmail.com>
2025-11-27 16:01:28 -0500

    Update Taurus CE data
    updated EBS, SO, MS

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Taurus/TaurusCommonMovement8.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Taurus/TaurusCommonMovement8.cs
@@ -6,6 +6,9 @@ public class TaurusCommonMovement8 : CommonEnemyMovement
 {
     protected override IEnumerator Move()
     {
+        yield return parentShip.MoveRelative(Vector3.down, Random.Range(3f, 4.5f), 1f);
+        yield return WaitForSeconds(5f);
+
         yield return LeaveScene();
     }
 }
--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Taurus/TaurusShooter010.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Taurus/TaurusShooter010.cs
@@ -4,19 +4,28 @@ using static CoroutineHelper;
 
 public class TaurusShooter010 : CommonEnemyShooter<EnemyBullet>
 {
-    const int BulletCount = 0;
+    const int BulletCount = 50;
+    const float BulletSpawnRadius = 2f;
+    const float BulletMinSpeed = 2f;
+    const float BulletMaxSpeed = 4f;
+
+    protected override float ShootingCooldown => 0.05f;
 
     protected override IEnumerator Shoot()
     {
-        while (enabled)
-        {
-            for (int i = 0; i < BulletCount; i++)
-            {
-                float z = 0;
-                Vector3 pos = Vector3.zero;
+        yield return WaitForSeconds(1f);
 
-                SpawnProjectile(0, z, pos).Fire();
-            }
+        for (int i = 0; i < BulletCount; i++)
+        {
+            float z = 0f;
+            float t = Random.value;
+            float s = Mathf.Lerp(BulletMinSpeed, BulletMaxSpeed, t);
+            Vector3 pos = BulletSpawnRadius * Random.insideUnitCircle;
+
+            bulletData.colour = bulletData.gradient.Evaluate(t);
+
+            var bullet = SpawnProjectile(7, z, pos);
+            bullet.StartCoroutine(bullet.LerpSpeed(0f, s, s * 0.5f));
 
             yield return WaitForSeconds(ShootingCooldown);
         }
