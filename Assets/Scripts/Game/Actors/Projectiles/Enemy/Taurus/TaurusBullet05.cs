e8b89a21c618a2f62c69687c30fe5377e1912a71
Yoshi1493 <nic.choy107@gmail.com>
2025-11-27 13:20:47 -0500

    Update TaurusBullet05.cs

--- Assets/Scripts/Game/Actors/Projectiles/Enemy/Taurus/TaurusBullet05.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Enemy/Taurus/TaurusBullet05.cs
@@ -1,7 +1,10 @@
 using System.Collections;
+using UnityEngine;
 
 public class TaurusBullet05 : MinibossBullet
 {
+    protected override int NumCollisions => Physics2D.OverlapBoxNonAlloc(transform.position, SpriteRenderer.size * 0.8f, transform.eulerAngles.z, collisionResults, CollisionMask);
+
     protected override float MaxLifetime => 8f;
 
     protected override IEnumerator Move()
