e76027179c88ba39ed10ddadd8bf812c3094a6d6
Yoshi1493 <nic.choy107@gmail.com>
2025-12-05 21:09:39 -0500

    Update Gemini CE data
    updated SO, MS

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Gemini/GeminiCommonMovement1.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Gemini/GeminiCommonMovement1.cs
@@ -6,8 +6,15 @@ public class GeminiCommonMovement1 : CommonEnemyMovement
 {
     protected override IEnumerator Move()
     {
-        yield return parentShip.MoveRelative(Vector3.down, 3f, 1.5f);
-        yield return WaitForSeconds(3f);
+        float d = -SignX;
+        Vector3 p0 = parentShip.transform.position;
+        Vector3 p1 = p0 + new Vector3(d * 4f, -6f);
+        Vector3 p2 = p1 + new Vector3(-d * 4f, 3f);
+
+        yield return parentShip.SpiralIntoPoint(p1, d * 30f, 1f);
+        yield return parentShip.SpiralIntoPoint(p2, d * 30f, 1f);
+        yield return WaitForSeconds(2f);
+        yield return parentShip.MoveRelative(Vector3.down, 5f, 1.8f);
 
         yield return LeaveScene();
     }
