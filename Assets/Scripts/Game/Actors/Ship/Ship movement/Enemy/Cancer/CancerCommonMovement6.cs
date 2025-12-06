bf0500c8f4a48b8987d5fa6ea103fd7dc9d8ad87
Yoshi1493 <nic.choy107@gmail.com>
2025-12-05 19:44:50 -0500

    Fix syntax consistency
    changed all instances of `<d> * <negative number>` to `<negative d> * <number>`

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Cancer/CancerCommonMovement6.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Cancer/CancerCommonMovement6.cs
@@ -9,9 +9,9 @@ public class CancerCommonMovement6 : CommonEnemyMovement
         Vector3 p0 = parentShip.transform.position;
         float d = SignX;
 
-        yield return parentShip.SpiralIntoPoint(p0 + new Vector3(d * -8f, -1.5f), d * 30f, 2f);
+        yield return parentShip.SpiralIntoPoint(p0 + new Vector3(-d * 8f, -1.5f), d * 30f, 2f);
         yield return WaitForSeconds(7f);
-        yield return parentShip.SpiralIntoPoint(parentShip.transform.position + new Vector3(d * -2f, 5f), d * 30f, 1.5f);
+        yield return parentShip.SpiralIntoPoint(parentShip.transform.position + new Vector3(-d * 2f, 5f), d * 30f, 1.5f);
 
         yield return LeaveScene();
     }
--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Cancer/CancerCommonMovement7.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Cancer/CancerCommonMovement7.cs
@@ -8,7 +8,7 @@ public class CancerCommonMovement7 : CommonEnemyMovement
         Vector3 p0 = parentShip.transform.position;
         float d = SignX;
 
-        yield return parentShip.SpiralIntoPoint(p0 + new Vector3(d * -2f, -13f), d * 30f, 6f);
+        yield return parentShip.SpiralIntoPoint(p0 + new Vector3(-d * 2f, -13f), d * 30f, 6f);
 
         yield return LeaveScene();
     }
