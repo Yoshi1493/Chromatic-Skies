0c8b3e17b179bd268bfe4f76a21ada155240b850
Yoshi1493 <nic.choy107@gmail.com>
2025-12-11 19:01:21 -0500

    Update Gemini CE data
    updated EB, EBS, SO, MS

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Gemini/GeminiCommonMovement3.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Gemini/GeminiCommonMovement3.cs
@@ -4,10 +4,20 @@ using static CoroutineHelper;
 
 public class GeminiCommonMovement3 : CommonEnemyMovement
 {
+    Player player;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        player = FindObjectOfType<Player>();
+    }
+
     protected override IEnumerator Move()
     {
-        yield return parentShip.MoveRelative(Vector3.down, 3f, 1.5f);
-        yield return WaitForSeconds(3f);
+        float d = Mathf.Sign(player.transform.position.x - parentShip.transform.position.x);
+
+        yield return parentShip.MoveRelative(Vector3.down.RotateVectorBy(d * 15f), Random.Range(3f, 5f), 1f);
+        yield return WaitForSeconds(4f);
 
         yield return LeaveScene();
     }
--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Gemini/GeminiShooter03.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Gemini/GeminiShooter03.cs
@@ -1,21 +1,37 @@
 using System.Collections;
 using UnityEngine;
 using static CoroutineHelper;
+using static MathHelper;
 
 public class GeminiShooter03 : CommonEnemyShooter<EnemyBullet>
 {
-    const int BulletCount = 0;
+    const int WaveCount = 32;
+    const int BulletCount = 2;
+    const float BulletSpacing = 0.32f;
+    const float BulletSpawnRadius = 2f;
+    const float BulletRotationSpeed = 64;
+    const float BulletRotationDuration = 1.6f;
+
+    protected override float ShootingCooldown => 0.05f;
 
     protected override IEnumerator Shoot()
     {
-        while (enabled)
-        {
-            for (int i = 0; i < BulletCount; i++)
-            {
-                float z = 0f;
-                Vector3 pos = Vector3.zero;
+        yield return WaitForSeconds(1f);
 
-                SpawnProjectile(0, z, pos).Fire();
+        for (int i = 0; i < WaveCount; i++)
+        {
+            float z = RandomAngleDeg;
+            float r = Random.Range(0.1f, 1f) * BulletSpawnRadius;
+
+            for (int ii = 0; ii < BulletCount; ii++)
+            {
+                int d = ii % 2 * 2 - 1;
+                Vector3 pos = ((r * transform.up) + (d * BulletSpacing * transform.right)).RotateVectorBy(z);
+                bulletData.colour = bulletData.gradient.Evaluate(ii);
+
+                var bullet = SpawnProjectile(3, z, pos);
+                bullet.StartCoroutine(bullet.RotateBy(d * BulletRotationSpeed, BulletRotationDuration, delay: 2.5f));
+                bullet.Fire();
             }
 
             yield return WaitForSeconds(ShootingCooldown);
