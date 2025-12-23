1abb5a8069fa97baa03b8541e0c2ee1003e5768f
Yoshi1493 <nic.choy107@gmail.com>
2025-12-22 19:38:35 -0500

    Update GeminiShooter06.cs

--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Gemini/GeminiShooter06.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Gemini/GeminiShooter06.cs
@@ -4,28 +4,36 @@ using static CoroutineHelper;
 
 public class GeminiShooter06 : CommonEnemyShooter<EnemyBullet>
 {
+    const int WaveCount = 16;
     const float WaveSpacing = 16f;
     const int BranchCount = 4;
     const float BranchSpacing = 360f / BranchCount;
     const float SpawnRadiusModifier = 0.04f;
 
-    protected override float ShootingCooldown => 0.4f;
+    protected override float ShootingCooldown => 0.2f;
 
     protected override IEnumerator Shoot()
     {
         yield return WaitForSeconds(1.5f);
 
-        for (int i = 0; enabled; i++)
+        while (enabled)
         {
-            for (int ii = 0; ii < BranchCount; ii++)
-            {
-                float z = (i * WaveSpacing) + (ii * BranchSpacing);
-                Vector3 pos = i * SpawnRadiusModifier * transform.up.RotateVectorBy(z);
+            float r = PlayerPosition.GetRotationDifference(transform.position);
 
-                SpawnProjectile(6, z, pos).Fire();
+            for (int i = 0; i < WaveCount; i++)
+            {
+                for (int ii = 0; ii < BranchCount; ii++)
+                {
+                    float z = (i * WaveSpacing) + (ii * BranchSpacing) + r;
+                    Vector3 pos = i * SpawnRadiusModifier * transform.up.RotateVectorBy(z);
+
+                    SpawnProjectile(6, z, pos).Fire();
+                }
+
+                yield return WaitForSeconds(ShootingCooldown);
             }
 
-            yield return WaitForSeconds(ShootingCooldown);
+            yield return WaitForSeconds(3f);
         }
     }
 }
