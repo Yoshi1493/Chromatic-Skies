fc7943c7cad3d8ef6f99534e17d788a95e565c8c
Yoshi1493 <nic.choy107@gmail.com>
2025-12-23 19:12:40 -0500

    Update Gemini CE data
    updated SO, MS

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Gemini/GeminiCommonMovement5.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Gemini/GeminiCommonMovement5.cs
@@ -15,7 +15,7 @@ public class GeminiCommonMovement5 : CommonEnemyMovement
         Vector3 p2 = new(-d * ScreenHalfWidth * 1.1f, p1.y + r);
 
         yield return parentShip.SpiralIntoPoint(p1, d * 30f, 1.2f);
-        yield return WaitForSeconds(5f);
+        yield return WaitForSeconds(8f);
         yield return parentShip.SpiralIntoPoint(p2, d * r * 20f, 1.5f);
 
         yield return LeaveScene();
