08ed38aee31f8332947f7ae7c1576e32f6f0837a
Yoshi1493 <nic.choy107@gmail.com>
2026-02-12 00:01:43 -0500

    Update Leo CE data
    updated EB, EBS, SO, MS

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Leo/LeoCommonMovement1.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Leo/LeoCommonMovement1.cs
@@ -7,7 +7,7 @@ public class LeoCommonMovement1 : CommonEnemyMovement
     protected override IEnumerator Move()
     {
         yield return parentShip.MoveRelative(Vector3.down, 3f, 1.5f);
-        yield return WaitForSeconds(3f);
+        yield return WaitForSeconds(5f);
 
         yield return LeaveScene();
     }
--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Leo/LeoShooter01.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Leo/LeoShooter01.cs
@@ -4,19 +4,26 @@ using static CoroutineHelper;
 
 public class LeoShooter01 : CommonEnemyShooter<EnemyBullet>
 {
-    const int BulletCount = 0;
+    const int WaveCount = 20;
+    const float BulletSpawnRadius = 2f;
+    const float BulletBaseSpeed = 4f;
 
     protected override IEnumerator Shoot()
     {
-        while (enabled)
-        {
-            for (int i = 0; i < BulletCount; i++)
-            {
-                float z = 0f;
-                Vector3 pos = Vector3.zero;
+        yield return WaitForSeconds(1.8f);
 
-                SpawnProjectile(0, z, pos).Fire();
-            }
+        float z = PlayerPosition.GetRotationDifference(transform.position);
+
+        for (int i = 0; i < WaveCount; i++)
+        {
+            Vector3 pos = BulletSpawnRadius * Vector3.forward;
+
+            bulletData.colour = bulletData.gradient.Evaluate(i / (WaveCount - 1f));
+
+            var bullet = SpawnProjectile(1, z, pos) as LeoBullet01;
+            bullet.rotationPoint = parentShip.transform.position;
+            bullet.MoveSpeed = BulletBaseSpeed;
+            bullet.Fire();
 
             yield return WaitForSeconds(ShootingCooldown);
         }
