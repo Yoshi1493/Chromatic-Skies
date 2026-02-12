8d64435f257e81df967822a8f3dc9ba9e5da3137
Yoshi1493 <nic.choy107@gmail.com>
2026-02-12 00:39:14 -0500

    Update LeoCommonMovement1.cs

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Leo/LeoCommonMovement1.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Leo/LeoCommonMovement1.cs
@@ -9,6 +9,10 @@ public class LeoCommonMovement1 : CommonEnemyMovement
         yield return parentShip.MoveRelative(Vector3.down, 3f, 1.5f);
         yield return WaitForSeconds(5f);
 
+        float d = -SignX;
+        Vector3 p1 = new(parentShip.transform.position.x + (d * 5f), -6f);
+
+        yield return parentShip.SpiralIntoPoint(p1, 30f, 4f);
         yield return LeaveScene();
     }
 }
