30d8ff4bf4f5ab9821ba8da7a2ab2a38b43bb6c1
Yoshi1493 <nic.choy107@gmail.com>
2026-02-14 00:45:02 -0500

    Update Leo CE data
    updated MS, prefab

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Leo/LeoCommonMovement2.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Leo/LeoCommonMovement2.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 
 public class LeoCommonMovement2 : CommonEnemyMovement
 {
+    [SerializeField] bool invertMovement;
+
     protected override IEnumerator Move()
     {
         Vector3 p0 = parentShip.transform.position;
         float d = SignX;
 
-        yield return parentShip.SpiralIntoPointLinear(Vector3.zero, -d * 30f, 2f);
-        yield return parentShip.SpiralIntoPointLinear(-p0, d * 30f, 2f);
+        yield return parentShip.SpiralIntoPointLinear(Vector3.zero, (invertMovement ? d : -d) * 30f, 2f);
+        yield return parentShip.SpiralIntoPointLinear(-p0, (invertMovement ? -d : d) * 30f, 2f);
 
         yield return LeaveScene();
     }
