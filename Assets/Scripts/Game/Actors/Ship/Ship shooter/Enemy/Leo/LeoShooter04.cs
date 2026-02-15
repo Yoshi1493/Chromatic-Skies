d65319976d43261e684d62e0e07b976f61ebced2
Yoshi1493 <nic.choy107@gmail.com>
2026-02-15 00:30:08 -0500

    Update Leo CE data
    updated EB, EBS, SO

--- Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Leo/LeoShooter04.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship shooter/Enemy/Leo/LeoShooter04.cs
@@ -4,21 +4,40 @@ using static CoroutineHelper;
 
 public class LeoShooter04 : CommonEnemyShooter<EnemyBullet>
 {
-    const int BulletCount = 0;
+    const int WaveCount = 16;
+    const float WaveSpacing = 360f / WaveCount;
+    const int BranchCount = 2;
+    const float BranchSpacing = 360f / BranchCount;
+    const int BulletCount = 8;
+    const float BulletSpacing = 360f / BulletCount;
+    const float BulletSpawnRadius = 0.16f;
+    const float BulletBaseSpeed = 2f;
 
     protected override IEnumerator Shoot()
     {
-        while (enabled)
+        yield return WaitForSeconds(1.2f);
+
+        for (int i = 0; i < WaveCount; i++)
         {
-            for (int i = 0; i < BulletCount; i++)
+            bulletData.colour = bulletData.gradient.Evaluate(Random.value);
+
+            for (int ii = 0; ii < BranchCount; ii++)
             {
-                float z = 0f;
-                Vector3 pos = Vector3.zero;
+                float z = (i * WaveSpacing) + (ii * BranchSpacing) + PlayerPosition.GetRotationDifference(transform.position);
 
-                SpawnProjectile(0, z, pos).Fire();
+                for (int iii = 0; iii < BulletCount; iii++)
+                {
+                    float t = (iii * BulletSpacing) + z;
+                    Vector3 pos = BulletSpawnRadius * transform.up.RotateVectorBy(t);
+
+                    var bullet = SpawnProjectile(0, z, pos);
+                    bullet.StartCoroutine(bullet.LerpSpeed(0f, BulletBaseSpeed, 2f));
+                    bullet.StartCoroutine(bullet.RotateBy(-iii * BulletSpacing, 0f, delay: 0.5f));
+                    bullet.Fire();
+                }
+
+                yield return WaitForSeconds(ShootingCooldown);
             }
-
-            yield return WaitForSeconds(ShootingCooldown);
         }
     }
 }
