8af619cfeaad0dd3104843d34815bb25ebe36ee1
Yoshi1493 <nic.choy107@gmail.com>
2026-02-16 01:51:46 -0500

    Update LeoCommonMovement4.cs

--- Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Leo/LeoCommonMovement4.cs
+++ Assets/Scripts/Game/Actors/Ship/Ship movement/Enemy/Leo/LeoCommonMovement4.cs
@@ -6,7 +6,7 @@ public class LeoCommonMovement4 : CommonEnemyMovement
 {
     protected override IEnumerator Move()
     {
-        yield return parentShip.MoveRelative(Vector3.down, 3f, 1.5f);
+        yield return parentShip.MoveRelative(Vector3.down.RotateVectorBy(Random.Range(-10f, 10f)), Random.Range(3f, 4f), 1.2f);
         yield return WaitForSeconds(3f);
 
         yield return LeaveScene();
