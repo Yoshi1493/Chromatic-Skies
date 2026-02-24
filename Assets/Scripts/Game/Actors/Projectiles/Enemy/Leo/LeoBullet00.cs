fc6d8012b90c7ff7a6589ad4962341b2ac96fdbf
Yoshi1493 <nic.choy107@gmail.com>
2026-02-24 11:46:42 -0500

    Update LeoBullet00.cs

--- Assets/Scripts/Game/Actors/Projectiles/Enemy/Leo/LeoBullet00.cs
+++ Assets/Scripts/Game/Actors/Projectiles/Enemy/Leo/LeoBullet00.cs
@@ -1,9 +1,23 @@
 using System.Collections;
+using UnityEngine;
 
 public class LeoBullet00 : EnemyBullet
 {
+    Vector2 originalSize;
+
+    protected override float MaxLifetime => 8f;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        originalSize = SpriteRenderer.size;
+    }
+
     protected override IEnumerator Move()
     {
-        yield return this.LerpSpeed(3f, 1.5f, 1.5f);
+        SpriteRenderer.size = originalSize * 0.25f;
+
+        StartCoroutine(this.LerpSize(originalSize, 0.25f));
+        yield return this.LerpSpeed(3f, 1.6f, 1.5f);
     }
 }
